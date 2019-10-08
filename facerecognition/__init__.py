0869a8d4757ff970aaae9000862f09d82d75efad
Lizaveta Kemerava <kemerava@mail.yu.edu>
2019-10-08 14:32:06 -0400

    Changed the paths and the responses. Jira task EGP-671

--- facerecognition/__init__.py
+++ facerecognition/__init__.py
@@ -1,6 +1,7 @@
 import imageio
 from flask import Flask, flash, request, redirect, jsonify
 from flasgger import Swagger, swag_from
+from http import HTTPStatus
 
 from facerecognition import image_helper, tf_helper, storage_factory, classifier
 from facerecognition.flasgger import template
@@ -12,7 +13,7 @@ api_key_header = 'X-Api-Key'
 retrain_param = 'retrain'
 
 
-@app.route('/save/<face_name>', methods=['POST'])
+@app.route('/faces/<face_name>', methods=['POST'])
 @swag_from('flasgger/upload_face.yaml')
 def upload_face(face_name):
     """
@@ -20,14 +21,14 @@ def upload_face(face_name):
     """
     if 'file' not in request.files:
         print('No file part')
-        return 'No file part', 400
+        return HTTPStatus.BAD_REQUEST
     file = request.files['file']
     if file.filename == '':
         print('No selected file')
-        return 'No selected file', 400
+        return HTTPStatus.BAD_REQUEST
     if api_key_header not in request.headers:
         print('Api key not specified')
-        return 'Api key not specified', 400
+        return HTTPStatus.FORBIDDEN
     api_key = request.headers[api_key_header]
     need_retrain = request.args.get(retrain_param, 'true')
     try:
@@ -39,8 +40,8 @@ def upload_face(face_name):
             classifier.train_async(api_key)
     except RuntimeError as e:
         print(str(e))
-        return str(e), 400
-    return '', 201
+        return HTTPStatus.INTERNAL_SERVER_ERROR
+    return HTTPStatus.CREATED
 
 
 @app.route('/recognize', methods=['POST'])
@@ -49,10 +50,10 @@ def recognize_faces():
 
     if 'file' not in request.files:
         flash('No file part')
-        return redirect(request.url), 400
+        return HTTPStatus.BAD_REQUEST
     if api_key_header not in request.headers:
         print('Api key not specified')
-        return 'Api key not specified', 400
+        return HTTPStatus.FORBIDDEN
     if 'limit' not in request.values or request.values['limit']=='':
         limit = -1
         print('Limit is not specified, find all faces')
@@ -63,7 +64,7 @@ def recognize_faces():
     file = request.files['file']
     if file.filename == '':
         print('No selected file')
-        return 'No selected file', 400
+        return HTTPStatus.BAD_REQUEST
     try:
         img = imageio.imread(file)
         face_img = image_helper.crop_faces(img, limit)
@@ -77,7 +78,7 @@ def recognize_faces():
         return jsonify(list_faces)
     except RuntimeError as e:
         print(str(e))
-        return str(e), 400
+        return HTTPStatus.INTERNAL_SERVER_ERROR
 
 
 @app.route('/retrain', methods=['POST'])
@@ -85,43 +86,44 @@ def recognize_faces():
 def retrain():
     if api_key_header not in request.headers:
         print('Api key not specified')
-        return 'Api key not specified', 400
+        return HTTPStatus.FORBIDDEN
     api_key = request.headers[api_key_header]
     try:
         classifier.train_async(api_key)
     except RuntimeError as e:
         print(str(e))
-        return str(e), 400
-    return '', 201
+        return HTTPStatus.INTERNAL_SERVER_ERROR
+    return HTTPStatus.CREATED
 
-@app.route('/all')
+@app.route('/faces')
 @swag_from('flasgger/get_all_names.yaml')
 def retrieveFaces():
     if api_key_header not in request.headers:
         print('Api key not specified')
-        return 'Api key not specified', 400
+        return HTTPStatus.FORBIDDEN
     try:
         api_key = request.headers[api_key_header]
-        return jsonify(classifier.get_face_name(api_key)), 200
+        return jsonify(classifier.get_face_name(api_key))
 
     except RuntimeError as e:
         print(str(e))
-        return str(e), 400
+        return HTTPStatus.INTERNAL_SERVER_ERROR
 
 
-@app.route('/remove/<face_name>', methods=['DELETE'])
+
+@app.route('/faces/<face_name>', methods=['DELETE'])
 @swag_from('flasgger/delete_record.yaml')
 def deleteRecord(face_name):
     if api_key_header not in request.headers:
         print('Api key not specified')
-        return 'Api key not specified', 400
+        return HTTPStatus.FORBIDDEN
     try:
         api_key = request.headers[api_key_header]
         classifier.delete_record(api_key, face_name)
     except RuntimeError as e:
         print(str(e))
-        return str(e), 400
-    return '', 204
+        return HTTPStatus.INTERNAL_SERVER_ERROR
+    return HTTPStatus.NO_CONTENT
 
 
 print("__name__ %s" % __name__)
