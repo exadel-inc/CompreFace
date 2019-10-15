e63066bf5854d529620092ef1437327c6f095b8c
Elijas Dapšauskas <edapsauskas@exadel.com>
2019-10-15 18:48:42 +0300

    Fix race condition in docker-test-build.sh

--- docker-test-build.sh
+++ docker-test-build.sh
@@ -1,17 +1,21 @@
 #!/bin/bash -xe
+# ===============================
+# docker-test-build.sh
+# Manually launched containerization and local deployment test
+# ===============================
 
 # File pre-processing (CRLF endings in certain files cause `docker-compose up` to crash)
 dos2unix ./*
 
 # Build and launch containers
 docker-compose build
-docker-compose up -d
+docker-compose up &
 
 # Run test
-curl --silent --fail http://localhost:5001/status
+timeout 600 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:5001/status)" != "200" ]]; do sleep 1; done' || false
 
 # Teardown
 docker-compose down
 
 # Confirm test result
-echo "TEST: SUCCESS"
+echo "TEST IS SUCCESSFUL"
