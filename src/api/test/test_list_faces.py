0454e5415381deb427edacaaaa3ae960cead6646
Elijas Dapšauskas <edapsauskas@exadel.com>
2019-10-15 19:28:20 +0300

    Add tests for "List Face Names" endpoint

--- src/api/test/test_list_faces.py
+++ src/api/test/test_list_faces.py
@@ -5,9 +5,24 @@ from src.api.test.constants import VALID_API_KEY
 
 
 def test__given_no_saved_faces__when_list_faces_is_requested__then_returns_empty_array(client, mocker):
-    pass
+    expected_names = []
+    mock = mocker.Mock()
+    mock.get_all_face_names.return_value = []
+    mocker.patch('src.api.main.get_storage', return_value=mock)
 
     res = client.get('/faces', headers={API_KEY_HEADER: VALID_API_KEY})
 
     assert res.status_code == HTTPStatus.OK
-    assert res.json['names'] == []
+    assert res.json['names'] == expected_names
+
+
+def test__given_saved_faces__when_list_faces_is_requested__then_returns_array_with_names(client, mocker):
+    expected_names = ['Joe Bloggs', 'Fred Bloggs']
+    mock = mocker.Mock()
+    mock.get_all_face_names.return_value = ['Joe Bloggs', 'Fred Bloggs']
+    mocker.patch('src.api.main.get_storage', return_value=mock)
+
+    res = client.get('/faces', headers={API_KEY_HEADER: VALID_API_KEY})
+
+    assert res.status_code == HTTPStatus.OK
+    assert res.json['names'] == expected_names
