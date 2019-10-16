aac70d2bc124afb92b0e35b9e39344c203775f60
Elijas Dapšauskas <edapsauskas@exadel.com>
2019-10-16 22:11:45 +0300

    Remove todo tags

--- src/facecrop/test/test_crop_faces.py
+++ src/facecrop/test/test_crop_faces.py
@@ -7,7 +7,7 @@ from src.facecrop import crop_faces
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
-def test__when_given_image_with_multiple_faces__then_returns_multiple_cropped_faces():  # TODO EGP-690
+def test__when_given_image_with_multiple_faces__then_returns_multiple_cropped_faces():
     im = imageio.imread(f'{SCRIPT_DIR}/files/multiple-faces.jpg')
 
     cropped_faces = crop_faces(im)
@@ -15,7 +15,7 @@ def test__when_given_image_with_multiple_faces__then_returns_multiple_cropped_fa
     assert len(cropped_faces) > 3
 
 
-def test__given_limit_3__when_given_image_with_multiple_faces__then_returns_3_cropped_faces():  # TODO EGP-690
+def test__given_limit_3__when_given_image_with_multiple_faces__then_returns_3_cropped_faces():
     im = imageio.imread(f'{SCRIPT_DIR}/files/multiple-faces.jpg')
 
     cropped_faces = crop_faces(im, face_lim=3)
