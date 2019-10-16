a62bb8d7feaf2ed0dda9d1edc2190a287186aa53
Lizaveta Kemerava <kemerava@mail.yu.edu>
2019-10-16 08:10:57 -0400

    WIP: working on add face test

--- src/api/test/test_add_face_example.py
+++ src/api/test/test_add_face_example.py
@@ -1,2 +1,16 @@
+from http import HTTPStatus
+
+from src.api.constants import API_KEY_HEADER
+from src.api.test.constants import VALID_API_KEY, SUCCESS_BODY
+
+FILE_BYTES = b''
+
 def test__when_add_face_example_endpoint_is_requested__then_preprocesses_and_saves_the_face(client):
-    ...  # TODO EGP-682
+    filename = 'test-file.jpg'
+    request_data = dict(file=(FILE_BYTES, filename))
+
+    res = client.post('/faces/New Face', headers={API_KEY_HEADER: VALID_API_KEY}, content_type='multipart/form-data', data=request_data)
+
+    assert res.status_code == HTTPStatus.OK
+    assert res.data.decode() == SUCCESS_BODY
+
