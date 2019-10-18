5820a0fba2003eb70b58d3d34c08d3bbcefc2633
Elijas Dapšauskas <edapsauskas@exadel.com>
2019-10-18 18:13:59 +0300

    Make Face Classifier to use readable data structures

--- src/faceclassifier/faceclassifier.py
+++ src/faceclassifier/faceclassifier.py
@@ -1,31 +1,20 @@
 import logging
 from threading import Thread
+from typing import List
 
 import imageio
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 
-from src import pyutils
 from src.database import get_storage
+from src.dto import BoundingBox
+from src.dto.cropped_face import CroppedFace
+from src.dto.face_prediction import FacePrediction
 from src.faceclassifier.exceptions import ThereIsNoModelForAPIKeyError
 from src.facecropper import crop_faces
 from src.faceembedder import calc_embedding
 
 models = {}
-
-@pyutils.run_once
-def train_all_models_once():
-    api_keys = get_storage().get_api_keys()
-    if not api_keys:
-        print("[WARNING] Face classifier training hasn't been started, because no API Keys were found in storage.")
-        exit(0)
-
-    for api_key in api_keys:
-        train(api_key)
-
-def train_async(api_key):
-    thread = Thread(target=train, daemon=False, args=[api_key])
-    thread.start()
 
 
 def train(api_key):
@@ -44,7 +33,23 @@ def train(api_key):
     }
 
 
-def classify_many(embedding, api_key, box):
+def train_async(api_key):
+    thread = Thread(target=train, daemon=False, args=[api_key])
+    thread.start()
+
+
+def train_all_models():
+    api_keys = get_storage().get_api_keys()
+    if not api_keys:
+        logging.warning("Face classifier training for all models hasn't been started, "
+                        "because no API Keys were found in storage.")
+        return
+
+    for api_key in api_keys:
+        train(api_key)
+
+
+def _classify_many(embedding, api_key, box: BoundingBox):
     if api_key not in models:
         raise ThereIsNoModelForAPIKeyError("There is no model for api key %s." % api_key)
     model_data = models[api_key]
@@ -52,25 +57,21 @@ def classify_many(embedding, api_key, box):
     logging.debug("predictions:")
     best_class_indices = np.argsort(-predictions)
     best_class_probability = predictions[best_class_indices[0]]
-    logging.debug('Best guess: %s with probability %.5f' % (
-        model_data["face_names"][best_class_indices[0]], best_class_probability))
-    logging.debug('Second guess: %s with probability %.5f' % (
-        model_data["face_names"][best_class_indices[1]], predictions[best_class_indices[1]]))
-    return {
-        "box_parameters": box,
-        "prediction": model_data["face_names"][best_class_indices[0]],
-        "probability": best_class_probability
-    }
+    logging.debug('Best guess: %s with probability %.5f', model_data["face_names"][best_class_indices[0]],
+                  best_class_probability)
+    logging.debug('Second guess: %s with probability %.5f', model_data["face_names"][best_class_indices[1]],
+                  predictions[best_class_indices[1]])
+    prediction = model_data["face_names"][best_class_indices[0]]
+    return FacePrediction(box=box, prediction=prediction, probability=best_class_probability)
 
 
 def recognize_faces(limit, file, api_key):
     img = imageio.imread(file)
-    face_img = crop_faces(img, limit)
+    faces: List[CroppedFace] = crop_faces(img, limit)
     recognized_faces = []
-    for face in range(0, len(face_img), 2):
-        embedding = calc_embedding(face_img[face])
-        face = classify_many(embedding, api_key, face_img[face + 1].tolist())
-        if face not in recognized_faces:
-            recognized_faces.append(face)
+    for face in faces:
+        embedding = calc_embedding(face.img)
+        recognized_face = _classify_many(embedding, api_key, face.box)
+        recognized_faces.append(recognized_face)
     logging.debug("The faces that were found:", recognized_faces)
     return recognized_faces
