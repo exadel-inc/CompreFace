ef976809aee3541deafaa37b338d62e4a14eea5c
Lizaveta Kemerava <kemerava@mail.yu.edu>
2019-10-22 06:21:22 -0400

    EGP-682 -- Unit tests

--- src/api/test/database_endpoints/test_remove_face.py
+++ src/api/test/database_endpoints/test_remove_face.py
@@ -1,2 +1,16 @@
-def test__when_remove_face_is_requested__then_removes_the_face(client):
-    ...  # TODO EGP-682 (should be very similar to test_retrain)
+from http import HTTPStatus
+
+from src.api.constants import API_KEY_HEADER
+from src.api.test.constants import VALID_API_KEY
+FILE_BYTES = b''
+
+
+
+def test__when_remove_face_example_is_requested__then_removes_the_face(client, mocker):
+    mock = mocker.Mock()
+    mock.remove_face.return_value = {"embeddings.model_name": "Model", "api_key": VALID_API_KEY, 'face_name': "John Smith"}
+    mocker.patch('src.api.controller.get_storage', return_value=mock)
+    res = client.delete('/faces/John Smith', headers={API_KEY_HEADER: VALID_API_KEY})
+    mock.remove_face.assert_called()
+
+    assert res.status_code == HTTPStatus.NO_CONTENT
