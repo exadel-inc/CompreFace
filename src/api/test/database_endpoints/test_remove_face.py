99e2e4d6dee0bba82ba165310551487b9d8d790b
Kemerava Lizaveta <lkemerava@exadel.com>
2019-10-22 17:02:58 +0000

    Apply suggestion to src/api/test/database_endpoints/test_remove_face.py

--- src/api/test/database_endpoints/test_remove_face.py
+++ src/api/test/database_endpoints/test_remove_face.py
@@ -13,5 +13,4 @@ def test__when_remove_face_is_requested__then_removes_the_face(client, mocker):
     res = client.delete('/faces/John Smith', headers={API_KEY_HEADER: VALID_API_KEY})
 
     mock.remove_face.assert_called()
-
     assert res.status_code == HTTPStatus.NO_CONTENT
