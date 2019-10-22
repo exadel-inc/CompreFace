09eee220f5d678fedd689fa6cff947ac4c6f80cb
Kemerava Lizaveta <lkemerava@exadel.com>
2019-10-22 17:02:35 +0000

    Apply suggestion to src/api/test/database_endpoints/test_remove_face.py

--- src/api/test/database_endpoints/test_remove_face.py
+++ src/api/test/database_endpoints/test_remove_face.py
@@ -8,7 +8,6 @@ FILE_BYTES = b''
 
 def test__when_remove_face_is_requested__then_removes_the_face(client, mocker):
     mock = mocker.Mock()
-    mock.remove_face.return_value = {"embeddings.model_name": "Model", "api_key": VALID_API_KEY, 'face_name': "John Smith"}
     mocker.patch('src.api.controller.get_storage', return_value=mock)
     res = client.delete('/faces/John Smith', headers={API_KEY_HEADER: VALID_API_KEY})
     mock.remove_face.assert_called()
