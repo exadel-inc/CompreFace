0a3e8bb2f816eb9ba9b55e0262673baac066820c
Elijas Dapšauskas <edapsauskas@exadel.com>
2019-10-24 18:49:57 +0300

    Add implementation of test get_api_keys with no api keys in DB

--- src/storage/test/conftest.py
+++ src/storage/test/conftest.py
@@ -1,16 +1,19 @@
 import pytest
 
+from src.storage._mongo_storage import MongoStorage
+from src.storage._mysql_storage import MySQLStorage
+
 STORAGE_MOCKS = ['mongo_storage_mock', 'mysql_storage_mock']
 
 
 @pytest.fixture
-def mongo_storage_mock():
-    return 'm'
+def mongo_storage_mock() -> MongoStorage:
+    return MongoStorage()
 
 
 @pytest.fixture
-def mysql_storage_mock():
-    return 'q'
+def mysql_storage_mock() -> MySQLStorage:
+    return MySQLStorage()
 
 
 @pytest.fixture
--- src/storage/test/test_add_face.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from src.storage.test.conftest import STORAGE_MOCKS
-
-
-@pytest.mark.parametrize('storage_mock', STORAGE_MOCKS, indirect=True)
-def test_(storage_mock):
-    assert storage_mock in 'qm'
