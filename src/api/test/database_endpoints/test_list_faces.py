d09ea4a4f887ccd52153845926615fb47079ca71
Lizaveta Kemerava <kemerava@mail.yu.edu>
2019-10-28 09:26:16 -0400

    EGP-710: removed unnecessary tests

--- src/api/test/database_endpoints/test_list_faces.py
+++ src/api/test/database_endpoints/test_list_faces.py
@@ -16,21 +16,4 @@ def test__given_certain_amount_of_saved_faces__when_list_faces_is_requested__the
     assert res.status_code == HTTPStatus.OK, res.json
     assert res.json['names'] == expected_names
 
-def test__given_no_saved_faces__when_list_faces_is_requested__then_returns_empty_array(client, mocker):
-    expected_names = []
-    mocker.patch('src.api.controller.get_storage', return_value=return_value_for_mock(mocker, expected_names))
 
-    res = client.get('/faces')
-
-    assert res.status_code == HTTPStatus.OK, res.json
-    assert res.json['names'] == expected_names
-
-
-def test__given_saved_faces__when_list_faces_is_requested__then_returns_array_with_names(client, mocker):
-    expected_names = ['Joe Bloggs', 'Fred Bloggs']
-    mocker.patch('src.api.controller.get_storage', return_value=return_value_for_mock(mocker, expected_names))
-
-    res = client.get('/faces')
-
-    assert res.status_code == HTTPStatus.OK, res.json
-    assert res.json['names'] == expected_names
