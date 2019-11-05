0ed63569cc8f63371f26286dcdf069a4b4dece86
Klipikov Maksym <mklipikov@exadel.com>
2019-11-05 09:46:21 +0000

    Update build-and-test.sh

--- build-and-test.sh
+++ build-and-test.sh
@@ -15,7 +15,7 @@ DONT_BUILD_CONTAINERS=''
 FREEZE_REQUIREMENTS=''
 
 curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
-chmod +x /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose ./wait-for
 apt install dos2unix -y 
 
 cat docker-compose.yml
