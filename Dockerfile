4ce8bcd3ec5730acbbd1f60acb9fd7d18eb7d6e8
Elijas Dapšauskas <edapsauskas@exadel.com>
2019-11-08 18:01:04 +0200

    Minor refactoring

--- Dockerfile
+++ Dockerfile
@@ -17,9 +17,10 @@ RUN chmod +x $DIR/wait-for-it.sh
 RUN mkdir $DIR/mongo_data
 
 # Install dependencies
-RUN apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 && \
-    echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" |tee /etc/apt/sources.list.d/mongodb-org-4.0.list && \
-    apt update && apt install -y mongodb-org-tools mongodb-org-shell
+RUN apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 \
+    && echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" |tee /etc/apt/sources.list.d/mongodb-org-4.0.list \
+    && apt update \
+    && apt install -y mongodb-org-tools mongodb-org-shell
 RUN pip3 --no-cache-dir install -r $DIR/requirements.txt
 
 # Expose port for uWSGI
--- build-and-test.sh
+++ build-and-test.sh
@@ -1,30 +1,26 @@
 #!/bin/bash -xe
+
+DONT_BUILD_CONTAINERS=''
+FREEZE_REQUIREMENTS=''
+HOST='http://localhost:5001'
+
 print_usage() {
   printf "
 Builds containers and runs all tests against them
 
-Usage: ./build-and-test.sh [-d] [-f]
+Usage: ./%s [-d] [-f] [-h <HOST:PORT>]
 Options:
-    -d      Don't build Docker containers (useful when changes are made only outside the already built containers)
-    -f      If tests run successfully, freeze versions and dependencies in requirements.txt (useful after manually adding new dependencies)
-"
+    -d      Don't build Docker containers (useful when changes are made only outside the already built containers).
+    -f      If tests run successfully, freeze versions and dependencies in requirements.txt (useful after manually adding new dependencies).
+    -h      Specify host by which the started container can be accessed, for example: -h http://10.130.66.131:5001. Default is %s.
+" "$(basename "$0")" "$HOST"
 }
 
-## Parse arguments
-DONT_BUILD_CONTAINERS=''
-FREEZE_REQUIREMENTS=''
-
-curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
-chmod +x /usr/bin/docker-compose 
-echo "deb http://ftp.de.debian.org/debian testing main" >> /etc/apt/sources.list
-echo 'APT::Default-Release "testing";' | sudo tee -a /etc/apt/apt.conf.d/00local
-apt install dos2unix python3.6 python3-pip -y 
-ln -sf python3.7 /usr/bin/python
-
-while getopts 'df' flag; do
+while getopts 'df:h' flag; do
   case "${flag}" in
   d) DONT_BUILD_CONTAINERS='true' ;;
   f) FREEZE_REQUIREMENTS='true' ;;
+  h) HOST="$OPTARG" ;;
   *)
     print_usage
     exit 1
@@ -32,7 +28,16 @@ while getopts 'df' flag; do
   esac
 done
 
-cd "${0%/*}" # Set Current Dir to the script's dir
+## Install dependencies
+curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose 
+echo "deb http://ftp.de.debian.org/debian testing main" >> /etc/apt/sources.list
+echo 'APT::Default-Release "testing";' | sudo tee -a /etc/apt/apt.conf.d/00local
+apt install dos2unix python3.6 python3-pip -y 
+ln -sf python3.7 /usr/bin/python
+
+## Set Current Dir to the script's dir
+cd "${0%/*}"
 
 ## Build and run docker containers
 dos2unix ./* # File pre-processing (CRLF endings in certain files cause `docker-compose up` to crash)
@@ -43,9 +48,8 @@ docker-compose up &
 trap "docker-compose down" SIGINT SIGTERM EXIT
 
 ## Wait until successful start of the service with 60s timeout
-export HOST=http://localhost:5001
-#timeout 60 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $HOST/status)" != "200" ]]; do sleep 1; echo "$HOST/status"; done'
-sleep 60
+export HOST
+timeout 60 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $HOST/status)" != "200" ]]; do sleep 1; echo "$HOST/status"; done'
 
 ## Run tests from inside the container
 docker exec ml python3 -m pytest -m "not integration" -ra --verbose src
@@ -53,7 +57,7 @@ docker exec ml python3 -m pytest -m integration -ra --verbose src
 
 ## Run E2E tests from outside the container
 python -m pip install requests pytest pytest-ordering
-python -m pytest --host http://10.130.66.131:5001 -ra --verbose test_e2e/test_e2e.py
+python -m pytest --host "$HOST" -ra --verbose test_e2e/test_e2e.py
 
 ## Freeze versions and dependencies in requirements.txt
 if [ "$FREEZE_REQUIREMENTS" = 'true' ]; then
