af20b1732dbedc8ff405c68f0b4a15e2bc5c2f5f
Elijas Dapšauskas <master.elijas@gmail.com>
2019-12-02 18:55:58 +0200

    Fix .env bug

--- src/init_runtime.py
+++ src/init_runtime.py
@@ -1,8 +1,8 @@
 import logging
 
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
 
 def init_runtime():
     logging.basicConfig(level=logging.DEBUG)
-    load_dotenv(verbose=True)
+    load_dotenv(find_dotenv(), verbose=True)
