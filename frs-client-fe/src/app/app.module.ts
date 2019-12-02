7a71a91ca26e3308304871bf6064f507989ab0d0
mrvladymyr2 <jikol4526363>
2019-12-02 14:34:10 +0200

    added angular material

--- frs-client-fe/src/app/app.module.ts
+++ frs-client-fe/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './ui/main-layout/main-layout.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { MainLayoutComponent } from './ui/main-layout/main-layout.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
