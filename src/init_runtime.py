cad8aab2fe3c5235d0885da64704fc071f50eb16
Elijas Dapšauskas <master.elijas@gmail.com>
2019-12-02 18:48:52 +0200

    Add .env support

--- src/init_runtime.py
+++ src/init_runtime.py
@@ -1,5 +1,8 @@
 import logging
 
+from dotenv import load_dotenv
+
 
 def init_runtime():
     logging.basicConfig(level=logging.DEBUG)
+    load_dotenv()
--- test_perf/recognition_accuracy_test.py
+++ test_perf/recognition_accuracy_test.py
@@ -1,6 +1,9 @@
+import logging
+
 from sklearn.datasets import fetch_lfw_people
 
 from src.face_recognition.crop_faces.exceptions import NoFaceFoundError
+from src.init_runtime import init_runtime
 from test_perf._data_wrangling import split_train_test, parse_lfw_data
 from test_perf._model_wrappers import PythonModel, ModelWrapperBase
 from test_perf.dto import Dataset
@@ -17,11 +20,12 @@ def calculate_accuracy(model: ModelWrapperBase, dataset: Dataset) -> float:
         try:
             model.add_example(img, name)
         except NoFaceFoundError as e:
-            print(str(e))
+            logging.warning(str(e))
     model.train()
     return sum(name == model.recognize(img) for name, img in dataset.test) / len(dataset.test)
 
 
 if __name__ == '__main__':
+    init_runtime()
     accuracy = calculate_accuracy(model=PythonModel(), dataset=get_lfw_dataset())
     print(f'Accuracy: {accuracy * 100}%')
