1712e5058587676b6fd72615679f0b28afadd392
Cheslavskyi Stanislav <scheslavskyi@exadel.com>
2019-12-05 15:27:44 +0200

    swagger fix

--- src/main/java/com/exadel/frs/controller/LoginController.java
+++ src/main/java/com/exadel/frs/controller/LoginController.java
@@ -8,9 +8,11 @@ import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,15 +22,14 @@ public class LoginController {
 
   private final UserService userService;
 
-  private final JwtTokenProvider jwtTokenProvider;
 
-  @PostMapping("/")
+  @PostMapping
   @ApiOperation(value = "Get jwt token")
   @ApiResponses({
       @ApiResponse(code = 401, message = "Authorisation not successful, access denied")
   })
-  public ResponseEntity<AccessToken> loginUser(@ApiParam(value = "Username", required = true) String username
-      , @ApiParam(value = "User password", required = true) String password) {
+  public ResponseEntity<AccessToken> loginUser(@ApiParam(value = "Username", required = true) @RequestParam String username
+      , @ApiParam(value = "User password", required = true) @RequestParam String password) {
     String token = userService.login(username, password);
     return ResponseEntity.ok(new AccessToken(token));
   }
