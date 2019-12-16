0720605cd0dce8a087c6aae1699aa5d8fabeaa40
sblashuk <sblashuk@exadel.com>
2019-12-16 14:44:42 +0300

    EFRS-112 Change exception message, when user doesn't belong to organisation

--- apps/frs-crud-app/src/main/java/com/exadel/frs/exception/UserDoesNotBelongToOrganization.java
+++ apps/frs-crud-app/src/main/java/com/exadel/frs/exception/UserDoesNotBelongToOrganization.java
@@ -5,10 +5,11 @@ import static java.lang.String.format;
 
 public class UserDoesNotBelongToOrganization extends BasicException {
 
-    public static final String MESSAGE = "User %s does not belong to organization %s";
+    public static final String MESSAGE = "The organization %s doesn't exist or " +
+            "organization exists, but user %s doesn't have view permission for that organization";
 
     public UserDoesNotBelongToOrganization(final Long userId, final Long organizationId) {
-        super(USER_DOES_NOT_BELONG_TO_ORGANIZATION, format(MESSAGE, userId, organizationId));
+        super(USER_DOES_NOT_BELONG_TO_ORGANIZATION, format(MESSAGE, organizationId, userId));
     }
 
 }
