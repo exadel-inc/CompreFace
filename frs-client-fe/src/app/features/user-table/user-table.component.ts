7704ec3831daf207834cd3d78196aa8c8c3825fc
Seramiazhka Anton <aseramiazhka@exadel.net>
2020-01-20 15:37:54 +0300

    EFRS-174 minor change

--- frs-client-fe/src/app/features/user-table/user-table.component.ts
+++ frs-client-fe/src/app/features/user-table/user-table.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { TableComponent } from '../table/table.component';
-import { Observable, of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-table',
@@ -13,6 +13,6 @@ export class UserTableComponent extends TableComponent implements OnInit {
   @Input() availableRoles$: Observable<string[]>;
 
   public isRoleChangeAllowed(userRole: string): Observable<boolean> {
-    return this.availableRoles$.pipe(switchMap(availableRoles => of(!!~availableRoles.indexOf(userRole))));
+    return this.availableRoles$.pipe(map(availableRoles => !!~availableRoles.indexOf(userRole)));
   }
 }
