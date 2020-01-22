3ea9b35ff5a0916994ace235892828b679059674
Seramiazhka Anton <aseramiazhka@exadel.net>
2020-01-22 13:27:25 +0300

    EFRS-174 fix merge issues

--- frs-client-fe/src/app/features/organization-header/organization-header.facade.ts
+++ frs-client-fe/src/app/features/organization-header/organization-header.facade.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
-import {OrganizationEnService} from "../../store/organization/organization-entitys.service";
-import {getSelectedOrganizationId, SelectUserRollForSelectedOrganization} from "../../store/organization/selectors";
-import {Store} from "@ngrx/store";
-import {Observable, Subscription} from "rxjs";
-import {Organization} from "../../data/organization";
-import {AppState} from "../../store";
-import {SetSelectedId} from "../../store/organization/action";
-import {ROUTERS_URL} from "../../data/routers-url.variable";
-import {Router} from "@angular/router";
-import {IFacade} from "../../core/facade/IFacade";
+import { OrganizationEnService } from "../../store/organization/organization-entitys.service";
+import { getSelectedOrganizationId, selectUserRollForSelectedOrganization } from "../../store/organization/selectors";
+import { Store } from "@ngrx/store";
+import { Observable, Subscription } from "rxjs";
+import { Organization } from "../../data/organization";
+import { AppState } from "../../store";
+import { SetSelectedId } from "../../store/organization/action";
+import { ROUTERS_URL } from "../../data/routers-url.variable";
+import { Router } from "@angular/router";
+import { IFacade } from "../../core/facade/IFacade";
 
 @Injectable()
-export class OrganizationHeaderFacade implements IFacade{
+export class OrganizationHeaderFacade implements IFacade {
   selectedId$: Observable<string | null>;
   selectedId: string | null;
   public userRole$: Observable<string | null>;
@@ -35,18 +35,18 @@ export class OrganizationHeaderFacade implements IFacade{
   }
 
   select(id: string) {
-    this.store.dispatch(new SetSelectedId({selectId: id}));
-    this.router.navigate([ROUTERS_URL.ORGANIZATION, id ])
+    this.store.dispatch(new SetSelectedId({ selectId: id }));
+    this.router.navigate([ROUTERS_URL.ORGANIZATION, id])
   }
 
   rename(name: string) {
-    this.organizationEnService.update({name, id: this.selectedId})
+    this.organizationEnService.update({ name, id: this.selectedId })
   }
 
   add(org) {
     this.organizationEnService.add(org).subscribe(org => {
-      if(org) {
-        this.store.dispatch(new SetSelectedId({selectId: org.id }));
+      if (org) {
+        this.store.dispatch(new SetSelectedId({ selectId: org.id }));
         this.router.navigate([ROUTERS_URL.ORGANIZATION, org.id])
       }
     });
