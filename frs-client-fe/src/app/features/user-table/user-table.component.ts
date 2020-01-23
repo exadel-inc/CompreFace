0b7ad1391c86a40deba60fc65bfd25c43aacc551
Seramiazhka Anton <aseramiazhka@exadel.net>
2020-01-23 13:03:57 +0300

    EFRS-174 MR remove role from invite user request; add onPush strategy for user-table

--- frs-client-fe/src/app/features/user-table/user-table.component.ts
+++ frs-client-fe/src/app/features/user-table/user-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 import { TableComponent } from '../table/table.component';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -6,10 +6,10 @@ import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-user-table',
   templateUrl: './user-table.component.html',
-  styleUrls: ['./user-table.component.sass']
+  styleUrls: ['./user-table.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserTableComponent extends TableComponent implements OnInit {
-  public availableRoles: string[];
   @Input() availableRoles$: Observable<string[]>;
 
   public isRoleChangeAllowed(userRole: string): Observable<boolean> {
