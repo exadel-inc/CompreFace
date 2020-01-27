7cf0ea51676a34e27514c73e3a041a067a391942
Seramiazhka Anton <aseramiazhka@exadel.net>
2020-01-27 11:55:16 +0300

    EFRS-179 fix tests

--- frs-client-fe/src/app/core/app-user/app-user.service.spec.ts
+++ frs-client-fe/src/app/core/app-user/app-user.service.spec.ts
@@ -1,9 +1,21 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
 
 import { AppUserService } from './app-user.service';
 
 describe('AppUserService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: AppUserService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [AppUserService]
+    });
+
+    service = TestBed.get(AppUserService);
+    httpMock = TestBed.get(HttpTestingController);
+  });
 
   it('should be created', () => {
     const service: AppUserService = TestBed.get(AppUserService);
--- frs-client-fe/src/app/features/app-user-list/application-user-list.component.spec.ts
+++ frs-client-fe/src/app/features/app-user-list/application-user-list.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ApplicationUserListComponent } from './application-user-list.component';
+import { SpinnerModule } from '../spinner/spinner.module';
+import { UserTableModule } from '../user-table/user-table.module';
+import { ApplicationUserListFacade } from './application-user-list-facade';
+import { of } from 'rxjs';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatFormFieldModule, MatInputModule, MatDialog } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ApplicationUserListComponent', () => {
   let component: ApplicationUserListComponent;
@@ -8,7 +15,27 @@ describe('ApplicationUserListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ApplicationUserListComponent ]
+      declarations: [ ApplicationUserListComponent ],
+      imports: [SpinnerModule, UserTableModule, ReactiveFormsModule, FormsModule, MatFormFieldModule, MatInputModule, NoopAnimationsModule],
+      providers: [{
+        provide: MatDialog,
+        useValue: {}
+      },{
+        provide: ApplicationUserListFacade,
+        useValue: {
+          initSubscriptions: () => of([{}]),
+          appUsers$: of([{
+            id: 0,
+            name: 'name',
+            owner: {
+              firstname: 'firstname'
+            }
+          }]),
+          selectedOrganization$: of([{}]),
+          isLoading$: of([{}]),
+          unsubscribe(){}
+        }
+      }]
     })
     .compileComponents();
   }));
