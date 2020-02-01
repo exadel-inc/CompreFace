902e232303c2f414d3304fb127948636302cdae0
Lizaveta Kemerava <kemerava@mail.yu.edu>
2020-02-01 11:03:05 -0500

    uwsgi parameters are set in the docker-compose

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,13 @@ export PYTHONUNBUFFERED=0
 
 ./wait-for-it.sh mongo:27017
 python3 ./init_mongo_db.py
-uwsgi --ini uwsgi.ini
+
+export memory="$UWSGI_MEMORY_REPORT"
+export lifetime="$UWSGI_MAX_LIFETIME"
+export requests="$UWSGI_MAX_REQUESTS"
+export memory_rss="$UWSGI_RELOAD_ON_RSS"
+export reload_mercy="$UWSGI_WORKER_RELOAD_MERCY"
+export memory_limit="$UWSGI_LIMIT_AS"
+
+uwsgi --ini uwsgi.ini --max-worker-lifetime "$lifetime" --memory-report  --max-requests "$requests"  --reload-on-rss "$memory_rss" --worker-reload-mercy "$reload_mercy" --limit-as "$memory_limit"
+
