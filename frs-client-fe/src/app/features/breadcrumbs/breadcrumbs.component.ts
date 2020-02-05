a0cad4d61562b4350895957f8b1c4b2fb7cad35f
mrvladymyr2 <jikol4526363>
2020-02-05 15:09:21 +0200

    small fix

--- frs-client-fe/src/app/features/breadcrumbs/breadcrumbs.component.ts
+++ frs-client-fe/src/app/features/breadcrumbs/breadcrumbs.component.ts
@@ -4,7 +4,6 @@ import {Observable} from "rxjs";
 import {Organization} from "../../data/organization";
 import { ROUTERS_URL } from "../../data/routers-url.variable";
 import {Application} from "../../data/application";
-import {Model} from "../../data/model";
 import {ActivatedRoute} from "@angular/router";
 import {filter, map} from "rxjs/operators";
 
@@ -16,14 +15,12 @@ import {filter, map} from "rxjs/operators";
 export class BreadcrumbsComponent implements OnInit {
   public org$: Observable<Organization>;
   public app$: Observable<Application>;
-  public model$: Observable<Model>;
   public routeModel$: Observable<string>;
   public ROUTERS_URL = ROUTERS_URL;
 
   constructor(private breadcrumbsFacade: BreadcrumbsFacade, private route: ActivatedRoute,) {
     this.org$ = breadcrumbsFacade.org$;
     this.app$ = breadcrumbsFacade.app$;
-    this.model$ = breadcrumbsFacade.model$;
     this.routeModel$ = route.data.pipe(
       map(data => data.routeName),
       filter(routeName => routeName === 'model')
