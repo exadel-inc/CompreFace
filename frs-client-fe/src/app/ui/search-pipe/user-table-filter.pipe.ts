baae09f075982f0d61311087fbcfda991b2201b4
mrvladymyr2 <jikol4526363>
2020-02-07 13:28:22 +0200

    prettify according to TSlint #5 (UI folders)

--- frs-client-fe/src/app/ui/search-pipe/user-table-filter.pipe.ts
+++ frs-client-fe/src/app/ui/search-pipe/user-table-filter.pipe.ts
@@ -1,22 +1,24 @@
-import {Pipe, PipeTransform} from "@angular/core";
-import {Observable} from "rxjs";
-import {map} from "rxjs/operators";
+import {Pipe, PipeTransform} from '@angular/core';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Pipe({
   name: 'userTableFilter'
 })
-export class UserTableFilterPipe implements PipeTransform{
+export class UserTableFilterPipe implements PipeTransform {
   transform(value: Observable<any>, search: string): Observable<any> {
-    if(!search.trim()) return value;
+    if (!search.trim()) {
+      return value;
+    }
 
     return value.pipe(
       map(e => {
         e.data = e.data.filter(row => {
-          return (row.firstName.toLocaleLowerCase()+ ' ' + row.lastName.toLocaleLowerCase()).includes(search.toLocaleLowerCase())
+          return (row.firstName.toLocaleLowerCase() + ' ' + row.lastName.toLocaleLowerCase()).includes(search.toLocaleLowerCase());
         });
         return e;
       })
-    )
+    );
   }
 
 }
