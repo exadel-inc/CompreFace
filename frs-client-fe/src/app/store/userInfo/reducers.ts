984952be5322131ef09438c32f60d5f61b2e44a7
Seramiazhka Anton <aseramiazhka@exadel.net>
2020-02-11 13:28:15 +0300

    fix test, minor changes

--- frs-client-fe/src/app/store/userInfo/reducers.ts
+++ frs-client-fe/src/app/store/userInfo/reducers.ts
@@ -27,38 +27,3 @@ export const UserInfoReducer = createReducer(initialState,
     ...state,
     ...action.user
   })));
-
-
-// export function UserInfoReducer(state = initialState, action: UserInfoActions): UserInfoState {
-//   switch(action.type) {
-//     case UserInfoActionTypes.UPDATE_USER_INFO: {
-//       return {
-//         ...state,
-//         ...action.payload
-//       };
-//     }
-
-//     case UserInfoActionTypes.RESET_USER_INFO: {
-//       return {
-//         ...initialState
-//       };
-//     }
-
-//     case UserInfoActionTypes.UPDATE_AUTHORIZED: {
-//       return {
-//         ...state,
-//         isAuthenticated: action.payload
-//       }
-//     }
-
-//     case UserInfoActionTypes.GET_USER_INFO_SUCCESS: {
-//       return {
-//         ...state,
-//         ...action.payload,
-//       };
-//     }
-
-//     default:
-//       return state;
-//   }
-// }
