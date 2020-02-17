c3f39502a5463550e1730c2662e9345d8b5e1740
Seramiazhka Anton <aseramiazhka@exadel.net>
2020-02-17 15:20:48 +0300

    add local env config

---
+++ frs-client-fe/src/environments/environment.local.ts
@@ -0,0 +1,8 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: false,
+  basicToken:  `Basic Q29tbW9uQ2xpZW50SWQ6cGFzc3dvcmQ=`,
+  // local mock server:
+  apiUrl: 'http://localhost:3000/'
+};
--- frs-client-fe/src/environments/environment.ts
+++ frs-client-fe/src/environments/environment.ts
@@ -7,9 +7,7 @@ import {Environment} from './interface';
 export const environment: Environment = {
   production: false,
   basicToken:  `Basic Q29tbW9uQ2xpZW50SWQ6cGFzc3dvcmQ=`,
-  // local mock server:
-  apiUrl: 'http://localhost:3000/'
-  // apiUrl: 'http://qa.frs.exadel.by:8080/'
+  apiUrl: 'http://dev.frs.exadel.by:8080/'
 };
 
 /*
