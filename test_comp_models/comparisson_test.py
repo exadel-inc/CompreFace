e2b2d326f7edc00ea1661724a9670a3ee015cc95
Lizaveta Kemerava <kemerava@mail.yu.edu>
2020-02-17 07:23:36 -0500

    Compare models test: more functions added

--- test_comp_models/comparisson_test.py
+++ test_comp_models/comparisson_test.py
@@ -11,6 +11,7 @@ from test_comp_models._model_wrappers import ModelWrapperBase, EfrsLocal_2018, E
     EfrsRestApi_Insightlib
 from test_perf.dto import Dataset, Datarows
 from typing import List, NamedTuple, Tuple
+import math
 
 CUR_DIR = Path(os.path.dirname(os.path.realpath(__file__)))
 
@@ -57,11 +58,34 @@ def create_folds():
                 pic = str(current[0]) + "_" + str(current[1].strip()) + ".jpg"
             img = CUR_DIR / "lfw" / current[0] / pic
             fold.append(Datarows(List[Tuple[current[0], imread(img)]]))
-            #fold[current[0]] = imread(img)
+            # fold[current[0]] = imread(img)
 
     return first_fold, second_fold, third_fold, forth_fold, fifth_fold, six_fold, \
            seventh_fold, eight_fold, nine_fold, ten_fold
 
+
+def calculate_accuracy(dataset: Dataset, model_1: ModelWrapperBase, model_2: ModelWrapperBase):
+    len_trainset = len(dataset.train)
+    undetected_1 = model_1.train(dataset.train)
+    undetected_2 = model_2.train(dataset.train)
+    detected_1 = len_trainset - undetected_1
+    detected_2 = len_trainset - undetected_2
+    detected_ratio_1 = detected_1 / len_trainset * 100
+    detected_ratio_2 = detected_2 / len_trainset * 100
+
+    len_testset = len(dataset.test)
+    recognized_1 = model_1.predict(dataset.test)
+    recognized_2 = model_2.predict(dataset.test)
+    recognized_ratio_1 = recognized_1 / detected_1
+    recognized_ratio_2 = recognized_2 / detected_2
+
+    print("Model #1 detection " + detected_1 + "/" + len_trainset + "(" + detected_ratio_1 + "%")
+    print("Model #2 detection " + detected_2 + "/" + len_trainset + "(" + detected_ratio_2 + "%")
+
+    print("Model #1 detection " + recognized_1 + "/" + detected_1 + "(" + recognized_ratio_1 + "%)")
+    print("Model #2 detection " + recognized_2 + "/" + detected_2 + "(" + recognized_ratio_2 + "%)")
+
+    return recognized_ratio_1, detected_1, recognized_ratio_2, detected_2
 
 
 if __name__ == '__main__':
@@ -76,6 +100,8 @@ if __name__ == '__main__':
 
     fold_1, fold_2, fold_3, fold_4, fold_5, fold_6, fold_7, fold_8, fold_9, fold_10 = create_folds()
     folds = [fold_1, fold_2, fold_3, fold_4, fold_5, fold_6, fold_7, fold_8, fold_9, fold_10]
+    list_recognized_1 = []
+    list_recognized_2 = []
     for experiment in range(len(folds)):
         train_set = []
         for fold in range(len(folds)):
@@ -84,4 +110,24 @@ if __name__ == '__main__':
             else:
                 train_set.append(folds[fold])
         dataset = Dataset(train=train_set, test=test_set)
+        print("fold #" + experiment)
+
+        recognized_1, detected_1, recognized_2, detected_2 = calculate_accuracy(dataset, model_1, model_2)
+        list_recognized_1.append(recognized_1)
+        list_recognized_2.append(recognized_2)
+
+    # idea: taking the size of the population as a 100, and then calculate accuracy based on that
+
+    mean_1 = sum(list_recognized_1) / len(list_recognized_1)
+    mean_2 = sum(list_recognized_2) / len(list_recognized_2)
+
+    numerator_1 = 0
+    numerator_2 = 0
+
+    for i in range(len(list_recognized_1)):
+        numerator_1 += (list_recognized_1[i] - mean_1) ** 2
+        numerator_2 += (list_recognized_2[i] - mean_2) ** 2
+
+    standard_deviation_1 = math.sqrt(numerator_1 / 100)
+    standard_deviation_2 = math.sqrt(numerator_2 / 100)
 
