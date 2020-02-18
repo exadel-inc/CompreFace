55a6d1aa3e391c4442662b51e60cbc01db2d6779
Vadim Amirov <vamirov@exadel.com>
2020-02-18 14:16:08 +0500

    EFRS-76: code clean up

--- apps/frs-core-app/src/main/java/com/exadel/frs/core/trainservice/filter/SecurityValidationFilter.java
+++ apps/frs-core-app/src/main/java/com/exadel/frs/core/trainservice/filter/SecurityValidationFilter.java
@@ -1,14 +1,12 @@
 package com.exadel.frs.core.trainservice.filter;
 
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-
+import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toMap;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -16,7 +14,8 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
 /**
  * Filter created to validate if this application has access to requested model
@@ -26,41 +25,44 @@ import javax.servlet.http.HttpServletRequest;
 @Order(1)
 public class SecurityValidationFilter implements Filter {
 
-  private static final String APP_KEY = "apikey";
-  private static final String MODEL_ID = "modelid";
+    private static final String APP_KEY = "apikey";
+    private static final String MODEL_ID = "modelid";
 
-  @Override
-  public void init(FilterConfig filterConfig) throws ServletException {
+    @Override
+    public void init(final FilterConfig filterConfig) {
 
-  }
-
-  @Override
-  public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
-      FilterChain filterChain) throws IOException, ServletException {
-    HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
-
-    Map<String, List<String>> headersMap = Collections
-        .list(httpRequest.getHeaderNames())
-        .stream()
-        .collect(Collectors.toMap(
-            Function.identity(),
-            h -> Collections.list(httpRequest.getHeaders(h))
-        ));
-
-    List<String> appKey = headersMap.getOrDefault(APP_KEY, Collections.emptyList());
-    List<String> modelId = headersMap.getOrDefault(MODEL_ID, Collections.emptyList());
-    if (appKey.size() > 1 || modelId.size() > 1) {
-      throw new RuntimeException("Multiple appkey or modelId exist");
     }
 
-    if (appKey.size() == 1 || modelId.size() == 1) {
-      //@todo security check
+    @Override
+    public void doFilter(
+            final ServletRequest servletRequest,
+            final ServletResponse servletResponse,
+            final FilterChain filterChain
+    ) throws IOException, ServletException {
+        var httpRequest = (HttpServletRequest) servletRequest;
+
+        Map<String, List<String>> headersMap = Collections
+                .list(httpRequest.getHeaderNames())
+                .stream()
+                .collect(toMap(
+                        Function.identity(),
+                        h -> Collections.list(httpRequest.getHeaders(h))
+                ));
+
+        var appKey = headersMap.getOrDefault(APP_KEY, emptyList());
+        var modelId = headersMap.getOrDefault(MODEL_ID, emptyList());
+        if (appKey.size() > 1 || modelId.size() > 1) {
+            throw new RuntimeException("Multiple appkey or modelId exist");
+        }
+
+        if (appKey.size() == 1 || modelId.size() == 1) {
+            //@todo security check
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
     }
-    filterChain.doFilter(servletRequest, servletResponse);
-  }
 
-  @Override
-  public void destroy() {
+    @Override
+    public void destroy() {
 
-  }
+    }
 }
