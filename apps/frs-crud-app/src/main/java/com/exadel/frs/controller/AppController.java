fcf662cb71842e908fccdc4982a2d21ebc9fd647
Dmitry Boiko <dboiko@exadel.com>
2020-02-19 14:57:56 +0200

    EFRS-261: Fixed import errors

--- apps/frs-crud-app/src/main/java/com/exadel/frs/controller/AppController.java
+++ apps/frs-crud-app/src/main/java/com/exadel/frs/controller/AppController.java
@@ -1,6 +1,7 @@
 package com.exadel.frs.controller;
 
 import static com.exadel.frs.system.global.Constants.GUID_EXAMPLE;
+
 import com.exadel.frs.dto.ui.AppCreateDto;
 import com.exadel.frs.dto.ui.AppResponseDto;
 import com.exadel.frs.dto.ui.AppUpdateDto;
@@ -17,8 +18,10 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+
 import java.util.List;
 import javax.validation.Valid;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -46,11 +49,9 @@ public class AppController {
     @ApiOperation(value = "Get application")
     public AppResponseDto getApp(
             @ApiParam(value = "GUID of organization", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String orgGuid,
+            @PathVariable final String orgGuid,
             @ApiParam(value = "GUID of application to return", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String guid
+            @PathVariable final String guid
     ) {
         return appMapper.toResponseDto(appService.getApp(guid, SecurityUtils.getPrincipalId()), SecurityUtils.getPrincipalId());
     }
@@ -59,8 +60,7 @@ public class AppController {
     @ApiOperation(value = "Get all applications in organization")
     public List<AppResponseDto> getApps(
             @ApiParam(value = "GUID of organization", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String orgGuid
+            @PathVariable final String orgGuid
     ) {
         return appMapper.toResponseDto(appService.getApps(orgGuid, SecurityUtils.getPrincipalId()), SecurityUtils.getPrincipalId());
     }
@@ -73,12 +73,10 @@ public class AppController {
     })
     public AppResponseDto createApp(
             @ApiParam(value = "GUID of organization", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String orgGuid,
+            @PathVariable final String orgGuid,
             @ApiParam(value = "Application object that needs to be created", required = true)
             @Valid
-            @RequestBody
-            final AppCreateDto appCreateDto
+            @RequestBody final AppCreateDto appCreateDto
     ) {
         return appMapper.toResponseDto(appService.createApp(appCreateDto, orgGuid, SecurityUtils.getPrincipalId()), SecurityUtils.getPrincipalId());
     }
@@ -87,15 +85,12 @@ public class AppController {
     @ApiOperation(value = "Update application")
     public AppResponseDto updateApp(
             @ApiParam(value = "GUID of organization", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String orgGuid,
+            @PathVariable final String orgGuid,
             @ApiParam(value = "GUID of application that needs to be updated", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String guid,
+            @PathVariable final String guid,
             @ApiParam(value = "Application data", required = true)
             @Valid
-            @RequestBody
-            final AppUpdateDto appUpdateDto
+            @RequestBody final AppUpdateDto appUpdateDto
     ) {
         return appMapper.toResponseDto(appService.updateApp(appUpdateDto, guid, SecurityUtils.getPrincipalId()), SecurityUtils.getPrincipalId());
     }
@@ -104,11 +99,9 @@ public class AppController {
     @ApiOperation(value = "Generate new api-key for application")
     public AppResponseDto regenerateApiKey(
             @ApiParam(value = "GUID of organization", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String orgGuid,
+            @PathVariable final String orgGuid,
             @ApiParam(value = "GUID of the application which api-key needs to be regenerated", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String guid
+            @PathVariable final String guid
     ) {
         appService.regenerateApiKey(guid, SecurityUtils.getPrincipalId());
 
@@ -119,11 +112,9 @@ public class AppController {
     @ApiOperation(value = "Delete application")
     public void deleteApp(
             @ApiParam(value = "GUID of organization", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String orgGuid,
+            @PathVariable final String orgGuid,
             @ApiParam(value = "GUID of the application that needs to be deleted", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String guid
+            @PathVariable final String guid
     ) {
         appService.deleteApp(guid, SecurityUtils.getPrincipalId());
     }
@@ -132,11 +123,9 @@ public class AppController {
     @ApiOperation(value = "Get application roles, that can be assigned to other users")
     public AppRole[] getOrgRolesToAssign(
             @ApiParam(value = "GUID of organization", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String orgGuid,
+            @PathVariable final String orgGuid,
             @ApiParam(value = "GUID of the application", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String guid
+            @PathVariable final String guid
     ) {
         return appService.getAppRolesToAssign(orgGuid, guid, SecurityUtils.getPrincipalId());
     }
@@ -145,14 +134,11 @@ public class AppController {
     @ApiOperation(value = "Get users of application")
     public List<UserRoleResponseDto> getAppUsers(
             @ApiParam(value = "GUID of organization", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String orgGuid,
+            @PathVariable final String orgGuid,
             @ApiParam(value = "GUID of application", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String guid,
+            @PathVariable final String guid,
             @ApiParam(value = "Filter by first name, last name or email of user")
-            @RequestParam(required = false)
-            final String search
+            @RequestParam(required = false) final String search
     ) {
         return userAppRoleMapper.toUserRoleResponseDto(appService.getAppUsers(search, orgGuid, guid, SecurityUtils.getPrincipalId()));
     }
@@ -162,15 +148,12 @@ public class AppController {
     @ApiOperation(value = "Invite user to application")
     public UserRoleResponseDto inviteUser(
             @ApiParam(value = "GUID of organization", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String orgGuid,
+            @PathVariable final String orgGuid,
             @ApiParam(value = "GUID of application", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String guid,
+            @PathVariable final String guid,
             @ApiParam(value = "Application object that needs to be created", required = true)
             @Valid
-            @RequestBody
-            final UserInviteDto userInviteDto
+            @RequestBody final UserInviteDto userInviteDto
     ) {
         return userAppRoleMapper.toUserRoleResponseDto(appService.inviteUser(userInviteDto, orgGuid, guid, SecurityUtils.getPrincipalId()));
     }
@@ -179,15 +162,12 @@ public class AppController {
     @ApiOperation(value = "Update user application role")
     public void updateUserAppRole(
             @ApiParam(value = "GUID of organization", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String orgGuid,
+            @PathVariable final String orgGuid,
             @ApiParam(value = "GUID of application", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String guid,
+            @PathVariable final String guid,
             @ApiParam(value = "User role data", required = true)
             @Valid
-            @RequestBody
-            final UserRoleUpdateDto userRoleUpdateDto
+            @RequestBody final UserRoleUpdateDto userRoleUpdateDto
     ) {
         appService.updateUserAppRole(userRoleUpdateDto, guid, SecurityUtils.getPrincipalId());
     }
--- apps/frs-crud-app/src/main/java/com/exadel/frs/controller/ModelController.java
+++ apps/frs-crud-app/src/main/java/com/exadel/frs/controller/ModelController.java
@@ -1,6 +1,7 @@
 package com.exadel.frs.controller;
 
 import static com.exadel.frs.system.global.Constants.GUID_EXAMPLE;
+
 import com.exadel.frs.dto.ui.ModelCreateDto;
 import com.exadel.frs.dto.ui.ModelResponseDto;
 import com.exadel.frs.dto.ui.ModelUpdateDto;
@@ -12,8 +13,10 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+
 import java.util.List;
 import javax.validation.Valid;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -39,14 +42,11 @@ public class ModelController {
     @ApiOperation(value = "Get model")
     public ModelResponseDto getModel(
             @ApiParam(value = "GUID of organization", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String orgGuid,
+            @PathVariable final String orgGuid,
             @ApiParam(value = "GUID of application", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String appGuid,
+            @PathVariable final String appGuid,
             @ApiParam(value = "GUID of model to return", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String guid
+            @PathVariable final String guid
     ) {
         return modelMapper.toResponseDto(
                 modelService.getModel(guid, SecurityUtils.getPrincipalId()),
@@ -58,11 +58,9 @@ public class ModelController {
     @ApiOperation(value = "Get all models in application")
     public List<ModelResponseDto> getModels(
             @ApiParam(value = "GUID of organization", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String orgGuid,
+            @PathVariable final String orgGuid,
             @ApiParam(value = "GUID of application", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String appGuid
+            @PathVariable final String appGuid
     ) {
         return modelMapper.toResponseDto(
                 modelService.getModels(appGuid, SecurityUtils.getPrincipalId()),
@@ -78,15 +76,12 @@ public class ModelController {
     })
     public ModelResponseDto createModel(
             @ApiParam(value = "GUID of organization", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String orgGuid,
+            @PathVariable final String orgGuid,
             @ApiParam(value = "GUID of application", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String appGuid,
+            @PathVariable final String appGuid,
             @ApiParam(value = "Model object that needs to be created", required = true)
             @Valid
-            @RequestBody
-            final ModelCreateDto modelCreateDto) {
+            @RequestBody final ModelCreateDto modelCreateDto) {
         return modelMapper.toResponseDto(
                 modelService.createModel(modelCreateDto, orgGuid, appGuid, SecurityUtils.getPrincipalId()),
                 appGuid
@@ -100,18 +95,14 @@ public class ModelController {
     })
     public void updateModel(
             @ApiParam(value = "GUID of organization", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String orgGuid,
+            @PathVariable final String orgGuid,
             @ApiParam(value = "GUID of application", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String appGuid,
+            @PathVariable final String appGuid,
             @ApiParam(value = "GUID of model that needs to be updated", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String guid,
+            @PathVariable final String guid,
             @ApiParam(value = "Model data", required = true)
             @Valid
-            @RequestBody
-            final ModelUpdateDto modelUpdateDto
+            @RequestBody final ModelUpdateDto modelUpdateDto
     ) {
         modelService.updateModel(modelUpdateDto, guid, SecurityUtils.getPrincipalId());
     }
@@ -120,14 +111,11 @@ public class ModelController {
     @ApiOperation(value = "Generate new api-key for model")
     public ModelResponseDto regenerateApiKey(
             @ApiParam(value = "GUID of organization", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String orgGuid,
+            @PathVariable final String orgGuid,
             @ApiParam(value = "GUID of application", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String appGuid,
+            @PathVariable final String appGuid,
             @ApiParam(value = "GUID of the model which GUID needs to be regenerated", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String guid
+            @PathVariable final String guid
     ) {
         modelService.regenerateApiKey(guid, SecurityUtils.getPrincipalId());
 
@@ -138,14 +126,11 @@ public class ModelController {
     @ApiOperation(value = "Delete model")
     public void deleteModel(
             @ApiParam(value = "GUID of organization", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String orgGuid,
+            @PathVariable final String orgGuid,
             @ApiParam(value = "GUID of application", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String appGuid,
+            @PathVariable final String appGuid,
             @ApiParam(value = "GUID of the model that needs to be deleted", required = true, example = GUID_EXAMPLE)
-            @PathVariable
-            final String guid
+            @PathVariable final String guid
     ) {
         modelService.deleteModel(guid, SecurityUtils.getPrincipalId());
     }
