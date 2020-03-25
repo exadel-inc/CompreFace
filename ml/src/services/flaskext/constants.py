dd9132713ce381150cf14186b581f6b69a4d9451
Dapšauskas Elijas <edapsauskas@exadel.com>
2020-03-25 18:02:46 +0200

    Remove redundant code from flaskext

--- ml/src/services/flaskext/constants.py
+++ ml/src/services/flaskext/constants.py
@@ -1,21 +1,3 @@
-from enum import auto
-
-from strenum import StrEnum
-
-API_KEY_HEADER = 'X-Api-Key'
-
-
 class ARG:
     LIMIT = 'limit'
     DET_PROB_THRESHOLD = 'det_prob_threshold'
-
-
-class GetParameter(StrEnum):
-    RETRAIN = auto()
-    FORCE = auto()
-
-
-class RetrainValue(StrEnum):
-    YES = auto()
-    NO = auto()
-    FORCE = auto()
