5194fcd174ef1a0805eec62272d1a73622e97c80
Dapšauskas Elijas <edapsauskas@exadel.com>
2020-03-27 06:28:22 +0200

    Add bug fix

--- ml/src/services/flaskext/test/test_needs_attached_file.py
+++ ml/src/services/flaskext/test/test_needs_attached_file.py
@@ -22,7 +22,7 @@ def client_with_file_endpoint(app):
 def test__given_no_file__when_requesting__then_returns_no_file_attached_error(client_with_file_endpoint):
     pass
 
-    res = client_with_file_endpoint.post('/endpoint', headers={API_KEY_HEADER: 'test-api-key'})
+    res = client_with_file_endpoint.post('/endpoint')
 
     assert res.status_code == HTTPStatus.BAD_REQUEST
     assert res.json['message'] == f"400 Bad Request: {NoFileAttachedError.description}"
@@ -32,7 +32,7 @@ def test__given_no_selected_file__when_requesting__then_returns_no_file_selected
     filename = ''
     request_data = dict(file=(FILE_BYTES, filename))
 
-    res = client_with_file_endpoint.post('/endpoint', headers={API_KEY_HEADER: 'test-api-key'}, data=request_data)
+    res = client_with_file_endpoint.post('/endpoint', data=request_data)
 
     assert res.status_code == HTTPStatus.BAD_REQUEST
     assert res.json['message'] == f"400 Bad Request: {NoFileSelectedError.description}"
@@ -42,7 +42,7 @@ def test__given_file__when_requesting__then_completes_request(client_with_file_e
     filename = 'test-file.xyz'
     request_data = dict(file=(FILE_BYTES, filename))
 
-    res = client_with_file_endpoint.post('/endpoint', headers={API_KEY_HEADER: 'test-api-key'}, data=request_data)
+    res = client_with_file_endpoint.post('/endpoint', data=request_data)
 
     assert res.status_code == HTTPStatus.OK, res.json
     assert res.data.decode() == 'success-body'
