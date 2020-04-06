3f791e7061b6d9725c84fabbf4323ec72a200c48
Elijas Dapšauskas <edapsauskas@exadel.com>
2020-04-06 06:12:35 +0300

    Fix test_oom.sh

--- ml/tools/test_oom.sh
+++ ml/tools/test_oom.sh
@@ -2,17 +2,17 @@
 IMG_DIR=$1
 
 SCANNERS=${SCANNERS:-Facenet2018}
-IMG_NAMES=${IMG_NAMES:-000_5.jpg 000_A.jpg 002_A.jpg 003_A.jpg 004_A.jpg 005_A.jpg 006_A.jpg}
-MEM_LIMITS=${MEM_LIMITS:-1g 2g 3g 4g 6g 8g}
-IMG_LENGTH_LIMITS=${IMG_LENGTH_LIMITS:-1000 99999}
+IMG_NAMES=${IMG_NAMES:-000_5.jpg 001_A.jpg 002_A.jpg 003_A.jpg 004_A.jpg 005_A.jpg 006_A.jpg}
+MEM_LIMITS=${MEM_LIMITS:-1g}
+IMG_LENGTH_LIMITS=${IMG_LENGTH_LIMITS:-640 99999}
 SHOW_OUTPUT=${SHOW_OUTPUT:-true}
 
 for scanner in ${SCANNERS/,/ }; do
-  for image_name in ${IMG_NAMES/,/ }; do
-    for mem_limit in ${MEM_LIMITS/,/ }; do
-      for img_length_limit in ${IMG_LENGTH_LIMITS/,/ }; do
+  for mem_limit in ${MEM_LIMITS/,/ }; do
+    for img_length_limit in ${IMG_LENGTH_LIMITS/,/ }; do
+      for img_name in ${IMG_NAMES/,/ }; do
         # Run test
-        OUTPUT=$(docker run --memory=$mem_limit --memory-swap=$mem_limit -e IMG_LENGTH_LIMIT=$img_length_limit "frs-core_ml${ID}" python -m tools.scan_faces "$scanner" "$image_name" 2>&1)
+        OUTPUT=$(docker run --memory=$mem_limit --memory-swap=$mem_limit -e IMG_LENGTH_LIMIT=$img_length_limit -e SCANNER=$scanner -e IMG_NAME=$img_name "frs-core_ml${ID}" python -m tools.scan_faces 2>&1)
         EXIT_CODE=$?
 
         # Format output
@@ -26,7 +26,7 @@ for scanner in ${SCANNERS/,/ }; do
         fi
 
         # Format and print result
-        MSG=$(identify -ping -format "%f ${img_length_limit}px $mem_limit %G %[size] ($scanner)\n" $IMG_DIR/$image_name)
+        MSG=$(identify -ping -format "%f ${img_length_limit}px $mem_limit %G %[size] ($scanner)\n" $IMG_DIR/$img_name)
         if [ $EXIT_CODE -eq 0 ]; then
           printf "[   OK   ] %s\n" "$MSG"
         else
