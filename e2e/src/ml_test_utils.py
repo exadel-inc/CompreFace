ec109cfdc5a8f2db76b3ad1daf46cc6658c12b0f
Elijas Dapšauskas <edapsauskas@exadel.com>
2020-04-11 04:24:51 +0300

    Fix bug

--- e2e/src/ml_test_utils.py
+++ e2e/src/ml_test_utils.py
@@ -1,8 +1,10 @@
+from typing import List
+
 from src import constants
 
 
-def embeddings_are_the_same(embedding1, embedding2):
-    assert embedding1.shape == embedding2.shape
+def embeddings_are_the_same(embedding1: List[float], embedding2: List[float]):
+    assert len(embedding1) == len(embedding2)
     for i in range(len(embedding1)):
         if (embedding1[i] - embedding2[i]) / embedding2[i] > constants.EMB_SIMILARITY_THRESHOLD:
             return False
