30410be7b74464e15919c40105d95048e1ed0ab7
Lizaveta Kemerava <kemerava@mail.yu.edu>
2020-04-13 09:50:05 -0400

    test for different formats finished

--- ml/src/services/imgtools/test/test_imageio_types.py
+++ ml/src/services/imgtools/test/test_imageio_types.py
@@ -1,6 +1,5 @@
-# jpeg, png, jpg, ico, bmp, gif, tif, tiff, webp
-import imageio
 import joblib
+import numpy
 import pytest
 
 from src.services.imgtools.read_img import read_img
@@ -11,7 +10,7 @@ CURRENT_DIR = get_dir(__file__)
 
 @pytest.mark.parametrize('img',
                          ["001_A.jpg", "001_A.jpeg", "001_A.png", "001_A.tiff", "001_A.ico", "001_A.gif", "001_A.bmp",
-                          "001_A.tif"])
+                          "001_A.tif", "001_A.webp"])
 def test_different_formats_processed_are_right(img):
     file = CURRENT_DIR / "test_files" / img
     actual_array = read_img(file)
@@ -19,4 +18,4 @@ def test_different_formats_processed_are_right(img):
     expected_array = joblib.load(load_array)
 
     assert (actual_array.shape == expected_array.shape)
-    assert (actual_array == expected_array).all()
+    assert numpy.allclose(actual_array, expected_array, atol=20, rtol=20)
