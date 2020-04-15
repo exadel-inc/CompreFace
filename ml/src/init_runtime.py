068212ca6667b004eaaca8953ab86bedfdac99a0
Elijas Dapšauskas <edapsauskas@exadel.com>
2020-04-15 14:00:28 +0300

    Make small changes

--- ml/src/init_runtime.py
+++ ml/src/init_runtime.py
@@ -1,4 +1,3 @@
-import os
 import sys
 
 import imageio
@@ -7,14 +6,8 @@ from PIL import ImageFile
 from src._logging import init_logging
 
 
-def _apply_imageio_fixes():
-    if os.environ.get('HOME') == '/root':  # TODO remove
-        os.environ['HOME'] = '/srv'  # TODO remove
-    imageio.plugins.freeimage.download()  # TODO remove
-    ImageFile.LOAD_TRUNCATED_IMAGES = True
-
-
 def init_runtime(logging_level):
     assert sys.version_info >= (3, 7)
-    _apply_imageio_fixes()
+    imageio.plugins.freeimage.download()
+    ImageFile.LOAD_TRUNCATED_IMAGES = True
     init_logging(logging_level)
