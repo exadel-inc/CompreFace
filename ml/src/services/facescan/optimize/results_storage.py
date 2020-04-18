5c5c725420c422b530c197853a8d672820454948
Elijas Dapšauskas <edapsauskas@exadel.com>
2020-04-18 15:25:52 +0300

    Refactor Facenet2018 parameter optimization

--- ml/src/services/facescan/optimize/results_storage.py
+++ ml/src/services/facescan/optimize/results_storage.py
@@ -1,21 +1,22 @@
+import time
 from pathlib import Path
 
 import joblib
-
-CHECKPOINT_FILENAME = Path('tmp') / 'checkpoint.joblib'
 
 
 class ResultsStorage:
     def __init__(self):
         self._scores = []
         self._total_scores = 0
+        timestamp_ms = int(round(time.time() * 1000))
+        self._checkpoint_filename = Path('tmp') / f'scores_top100_{timestamp_ms}.joblib'
 
     def save(self):
         self._scores = sorted(self._scores, key=lambda x: x.cost)[:100]
-        joblib.dump(self._scores, CHECKPOINT_FILENAME)
+        joblib.dump(self._scores, self._checkpoint_filename)
         print(f"[Best out of {self._total_scores}]:"
               f" Cost = {self._scores[0].cost} <- {tuple(self._scores[0].args)}."
-              f" Saved top 100 to '{CHECKPOINT_FILENAME}'.", flush=True)
+              f" Saved top 100 to '{self._checkpoint_filename}'.", flush=True)
 
     def add_score(self, score):
         self._scores.append(score)
--- ml/src/services/facescan/optimize/run.py
+++ ml/src/services/facescan/optimize/run.py
@@ -19,8 +19,6 @@ Score = namedtuple('Score', 'cost args')
 
 cached_read_img = cached(read_img)
 
-ARG_COUNT = 4
-
 
 class _ENV(Constants):
     LOGGING_LEVEL_NAME = ENV.LOGGING_LEVEL_NAME
@@ -29,6 +27,7 @@ class _ENV(Constants):
 
 class Facenet2018ThresholdOptimization:
     def __init__(self):
+        self.arg_count = 4
         self.scanner = FaceScanners.Facenet2018()
         self.dataset = [row for row in SAMPLE_IMAGES if row.img_name in _ENV.IMG_NAMES]
         logging.getLogger('src.services.facescan.scanner.test.calculate_errors').setLevel(logging.WARNING)
@@ -50,16 +49,16 @@ class Facenet2018ThresholdOptimization:
         return total_errors
 
 
-def plausible_args_iterator():
+def get_plausible_thresholds_iterator(arg_count):
     one_arg_values = [0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.99]
-    all_arg_values = list(itertools.product(one_arg_values, repeat=ARG_COUNT))
+    all_arg_values = list(itertools.product(one_arg_values, repeat=arg_count))
     random.shuffle(all_arg_values)
     return all_arg_values
 
 
-def infinite_random_args_gen():
+def random_thresholds_generator(arg_count):
     while True:
-        yield [random.uniform(0, 1) for _ in range(ARG_COUNT)]
+        yield [random.uniform(0, 1) for _ in range(arg_count)]
 
 
 if __name__ == '__main__':
@@ -67,8 +66,12 @@ if __name__ == '__main__':
     logging.info(_ENV.to_json() if ENV.IS_DEV_ENV else _ENV.to_str())
 
     task = Facenet2018ThresholdOptimization()
+    threshold_iterators = [
+        get_plausible_thresholds_iterator(task.arg_count),
+        random_thresholds_generator(task.arg_count)
+    ]
+
     storage = ResultsStorage()
     optimizer = Optimizer(task, storage, checkpoint_every_s=120)
-
-    optimizer.optimize(plausible_args_iterator())
-    optimizer.optimize(infinite_random_args_gen())
+    for threshold_iterator in threshold_iterators:
+        optimizer.optimize(threshold_iterator)
