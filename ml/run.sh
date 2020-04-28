f4a8daa1763bf299167d6b98fd3fa1c055ede69c
Dapšauskas Elijas <edapsauskas@exadel.com>
2020-04-28 21:42:25 +0300

    Doublequote to prevent word splitting in run.sh

--- ml/run.sh
+++ ml/run.sh
@@ -7,16 +7,16 @@ if [[ "$COMMAND" != "stop" && "$COMMAND" != "start" ]]; then
   exit 1
 fi
 
-if [ -f ${COMPOSE_PROJECT_NAME:-frs-core}.pid ]; then
-  kill -9 "$(cat ${COMPOSE_PROJECT_NAME:-frs-core}.pid)"
+if [ -f "${COMPOSE_PROJECT_NAME:-frs-core}".pid ]; then
+  kill -9 "$(cat "${COMPOSE_PROJECT_NAME:-frs-core}".pid)"
 fi
 
 if [[ "$COMMAND" == "start" ]]; then
   (
     python -m src.app &
-    echo $! >${COMPOSE_PROJECT_NAME:-frs-core}.pid
+    echo $! >"${COMPOSE_PROJECT_NAME:-frs-core}".pid
     wait
-    rm -f ${COMPOSE_PROJECT_NAME:-frs-core}.pid
+    rm -f "${COMPOSE_PROJECT_NAME:-frs-core}".pid
   ) &
   exit 0
 fi
