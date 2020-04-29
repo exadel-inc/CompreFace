287da33b7a416b9816d89e8b9151bc1ae4a23a26
valijon <vsobirov@exadel.com>
2020-04-29 10:50:56 +0300

    EFRS-455: Styled code

--- apps/frs-crud-app/src/test/java/com/exadel/frs/UserServiceTest.java
+++ apps/frs-crud-app/src/test/java/com/exadel/frs/UserServiceTest.java
@@ -11,34 +11,20 @@ import com.exadel.frs.exception.UserDoesNotExistException;
 import com.exadel.frs.helpers.EmailSender;
 import com.exadel.frs.repository.UserRepository;
 import com.exadel.frs.service.UserService;
-import liquibase.integration.spring.SpringLiquibase;
 import lombok.val;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.function.Executable;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.MockBeans;
-import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.mock.env.MockEnvironment;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
-import java.util.UUID;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -253,66 +239,5 @@ class UserServiceTest {
         val createdUser = userService.createUser(userCreateDto);
 
         assertEquals(userCreateDto.getEmail().toLowerCase(), createdUser.getEmail());
-    }
-
-    @Nested
-    @DisplayName("Tests that use database")
-    @ExtendWith(SpringExtension.class)
-    @DataJpaTest
-    @MockBeans({@MockBean(SpringLiquibase.class), @MockBean(PasswordEncoder.class), @MockBean(EmailSender.class)})
-    @Import({UserService.class})
-    public class RepositoryEnabledTest {
-        private static final String ENABLED_USER_EMAIL = "enabled_user@email.com";
-        private static final String DISABLED_USER_EMAIL = "disabled_user@email.com";
-
-        @SpyBean
-        private UserService userService;
-
-        @Autowired
-        private UserRepository userRepository;
-
-        @Test
-        void getEnabledUserByEmailReturnsActiveUser() {
-            createAndEnableUser(ENABLED_USER_EMAIL);
-
-            val enabledUser = userService.getEnabledUserByEmail(ENABLED_USER_EMAIL);
-
-            assertNotNull(enabledUser);
-            assertTrue(enabledUser.isEnabled());
-        }
-
-        @Test
-        void getEnabledUserByEmailThrowsExceptionIfUserIsDisabled() {
-            createUser(DISABLED_USER_EMAIL);
-
-            val disabledUser = userRepository.findByEmail(DISABLED_USER_EMAIL).get();
-
-            assertNotNull(disabledUser);
-            assertFalse(disabledUser.isEnabled());
-
-            assertThrows(UserDoesNotExistException.class, () -> userService.getEnabledUserByEmail(DISABLED_USER_EMAIL));
-        }
-
-        private void createAndEnableUser(String email) {
-            String regToken = UUID.randomUUID().toString();
-            when(userService.generateRegistrationToken()).thenReturn(regToken);
-            createUser(email);
-            confirmRegistration(regToken);
-        }
-
-        private void createUser(String email) {
-            val user = UserCreateDto.builder()
-                    .email(email)
-                    .firstName("first_name")
-                    .lastName("last_name")
-                    .password("password")
-                    .build();
-
-            userService.createUser(user);
-        }
-
-        private void confirmRegistration(String regToken) {
-            userService.confirmRegistration(regToken);
-        }
     }
 }
