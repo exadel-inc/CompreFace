a9abd4ec119c8c30a5a129b2535f758bb39094f7
akavalchuk <akavalchuk@exadel.com>
2020-04-29 19:08:08 +0300

    EFRS-425 Wrong request type sends when invite user to application from UI

--- frs-client-fe/src/app/features/user-list/user-list-facade.ts
+++ frs-client-fe/src/app/features/user-list/user-list-facade.ts
@@ -1,110 +1,124 @@
-import {Injectable} from '@angular/core';
-import {IFacade} from 'src/app/data/facade/IFacade';
-import {AppUser} from 'src/app/data/appUser';
-import {AppState} from 'src/app/store';
-import {Observable, Subscription, combineLatest} from 'rxjs';
-import {Store} from '@ngrx/store';
-import {selectCurrentOrganizationId} from 'src/app/store/organization/selectors';
-import {selectUsers, selectIsPendingUserStore} from 'src/app/store/user/selectors';
-import {selectAllRoles, selectIsPendingRoleStore} from 'src/app/store/role/selectors';
-import {selectUserRollForSelectedOrganization, selectSelectedOrganization} from 'src/app/store/organization/selectors';
-import {map, tap} from 'rxjs/operators';
-import {LoadUsersEntityAction, PutUpdatedUserRoleEntityAction, DeleteUserFromOrganization} from 'src/app/store/user/action';
-import {LoadRolesEntityAction} from 'src/app/store/role/actions';
-import {UserService} from 'src/app/core/user/user.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IFacade } from 'src/app/data/facade/IFacade';
+import { AppUser } from 'src/app/data/appUser';
+import { AppState } from 'src/app/store';
+import { Observable, Subscription, combineLatest, of } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { selectCurrentOrganizationId } from 'src/app/store/organization/selectors';
+import { selectUsers, selectIsPendingUserStore } from 'src/app/store/user/selectors';
+import { selectAllRoles, selectIsPendingRoleStore } from 'src/app/store/role/selectors';
+import {
+    selectUserRollForSelectedOrganization,
+    selectSelectedOrganization
+} from 'src/app/store/organization/selectors';
+import { catchError, map, tap } from 'rxjs/operators';
+import {
+    LoadUsersEntityAction,
+    PutUpdatedUserRoleEntityAction,
+    DeleteUserFromOrganization
+} from 'src/app/store/user/action';
+import { LoadRolesEntityAction } from 'src/app/store/role/actions';
+import { UserService } from 'src/app/core/user/user.service';
 
 @Injectable()
 export class UserListFacade implements IFacade {
-  public selectedOrganization$: Observable<string>;
-  public selectedOrganizationName$: Observable<string>;
-  public users$: Observable<AppUser[]>;
-  public availableRoles$: Observable<string[]>;
-  public isLoading$: Observable<boolean>;
+    public selectedOrganization$: Observable<string>;
+    public selectedOrganizationName$: Observable<string>;
+    public users$: Observable<AppUser[]>;
+    public availableRoles$: Observable<string[]>;
+    public isLoading$: Observable<boolean>;
 
-  private selectedOrganization: string;
+    private selectedOrganization: string;
 
-  private selectedOrganizationSubscription: Subscription;
+    private selectedOrganizationSubscription: Subscription;
 
-  constructor(private store: Store<AppState>, private userService: UserService) {
-    this.selectedOrganization$ = store.select(selectCurrentOrganizationId);
-    this.selectedOrganizationName$ = store.select(selectSelectedOrganization).pipe(map(org => org.name));
-    this.users$ = store.select(selectUsers);
+    constructor
+    (
+        private store: Store<AppState>,
+        private userService: UserService,
+    ) {
+        this.selectedOrganization$ = store.select(selectCurrentOrganizationId);
+        this.selectedOrganizationName$ = store.select(selectSelectedOrganization).pipe(map(org => org.name));
+        this.users$ = store.select(selectUsers);
 
-    const allRoles$ = store.select(selectAllRoles);
-    const userRoleInSelectedOrganization$ = store.select(selectUserRollForSelectedOrganization);
+        const allRoles$ = store.select(selectAllRoles);
+        const userRoleInSelectedOrganization$ = store.select(selectUserRollForSelectedOrganization);
 
-    this.availableRoles$ = combineLatest(
-      allRoles$,
-      userRoleInSelectedOrganization$
-    )
-      .pipe(
-        map(
-          (observResult) => {
-            let res = [];
-            const [allRoles, userRole] = observResult;
-            const roleIndex = allRoles.indexOf(userRole);
-
-            if (roleIndex !== -1) {
-              res = allRoles.slice(roleIndex);
-            }
-
-            return res;
-          }
+        this.availableRoles$ = combineLatest(
+            allRoles$,
+            userRoleInSelectedOrganization$
         )
-      );
+            .pipe(
+                map(
+                    (observResult) => {
+                        let res = [];
+                        const [allRoles, userRole] = observResult;
+                        const roleIndex = allRoles.indexOf(userRole);
 
-    const usersLoading$ = store.select(selectIsPendingUserStore);
-    const roleLoading$ = store.select(selectIsPendingRoleStore);
+                        if (roleIndex !== -1) {
+                            res = allRoles.slice(roleIndex);
+                        }
 
-    this.isLoading$ = combineLatest(usersLoading$, roleLoading$)
-          .pipe(map(observResults => !(!observResults[0] && !observResults[1])));
-  }
+                        return res;
+                    }
+                )
+            );
 
-  public initSubscriptions(): void {
-    this.selectedOrganizationSubscription = this.selectedOrganization$.subscribe(
-      orgId => {
-        if (orgId) {
-          this.selectedOrganization = orgId;
-          this.loadUsers();
-          this.loadAvailableRoles();
-        }
-      }
-    );
-  }
+        const usersLoading$ = store.select(selectIsPendingUserStore);
+        const roleLoading$ = store.select(selectIsPendingRoleStore);
 
-  public loadUsers(): void {
-    this.store.dispatch(LoadUsersEntityAction({
-      organizationId: this.selectedOrganization
-    }));
-  }
+        this.isLoading$ = combineLatest(usersLoading$, roleLoading$)
+            .pipe(map(observResults => !(!observResults[0] && !observResults[1])));
+    }
 
-  public updateUserRole(id: string, role: string): void {
-    this.store.dispatch(PutUpdatedUserRoleEntityAction({
-      organizationId: this.selectedOrganization,
-      user: {
-        id,
-        role
-      }
-    }));
-  }
+    public initSubscriptions(): void {
+        this.selectedOrganizationSubscription = this.selectedOrganization$.subscribe(
+            orgId => {
+                if (orgId) {
+                    this.selectedOrganization = orgId;
+                    this.loadUsers();
+                    this.loadAvailableRoles();
+                }
+            }
+        );
+    }
 
-  public deleteUser(userId: string): void {
-    this.store.dispatch(DeleteUserFromOrganization({
-      organizationId: this.selectedOrganization,
-      userId
-    }))
-  }
+    public loadUsers(): void {
+        this.store.dispatch(LoadUsersEntityAction({
+            organizationId: this.selectedOrganization
+        }));
+    }
 
-  public inviteUser(userEmail: string, role: string): Observable<any> {
-    return this.userService.inviteUser(this.selectedOrganization, userEmail, role)
-      .pipe(tap(() => this.loadUsers()));
-  }
+    public updateUserRole(id: string, role: string): void {
+        this.store.dispatch(PutUpdatedUserRoleEntityAction({
+            organizationId: this.selectedOrganization,
+            user: {
+                id,
+                role
+            }
+        }));
+    }
 
-  public loadAvailableRoles(): void {
-    this.store.dispatch(LoadRolesEntityAction());
-  }
+    public deleteUser(userId: string): void {
+        this.store.dispatch(DeleteUserFromOrganization({
+            organizationId: this.selectedOrganization,
+            userId
+        }))
+    }
 
-  public unsubscribe(): void {
-    this.selectedOrganizationSubscription.unsubscribe();
-  }
+    public inviteUser(userEmail: string, role: string): Observable<any> {
+        return this.userService.inviteUser(this.selectedOrganization, userEmail, role)
+            .pipe(
+                tap(() => this.loadUsers()),
+            );
+    }
+
+    public loadAvailableRoles(): void {
+        this.store.dispatch(LoadRolesEntityAction());
+    }
+
+    public unsubscribe(): void {
+        this.selectedOrganizationSubscription.unsubscribe();
+    }
 }
--- frs-client-fe/src/app/features/user-list/user-list.component.ts
+++ frs-client-fe/src/app/features/user-list/user-list.component.ts
@@ -1,98 +1,106 @@
-import {ChangeDetectionStrategy, Component, OnInit, OnDestroy} from '@angular/core';
-import {UserListFacade} from './user-list-facade';
-import {Observable, Subscription} from 'rxjs';
-import {AppUser} from 'src/app/data/appUser';
-import {map, switchMap, take} from 'rxjs/operators';
-import {ITableConfig} from '../table/table.component';
-import {SnackBarService} from '../snackbar/snackbar.service';
-import {InviteDialogComponent} from '../invite-dialog/invite-dialog.component';
-import {MatDialog} from '@angular/material';
-import {DeleteDialogComponent} from '../delete-dialog/delete-dialog.component';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component, OnInit, OnDestroy } from '@angular/core';
+import { UserListFacade } from './user-list-facade';
+import { Observable, of, Subscription } from 'rxjs';
+import { AppUser } from 'src/app/data/appUser';
+import { catchError, filter, map, switchMap, take, tap } from 'rxjs/operators';
+import { ITableConfig } from '../table/table.component';
+import { SnackBarService } from '../snackbar/snackbar.service';
+import { InviteDialogComponent } from '../invite-dialog/invite-dialog.component';
+import { MatDialog } from '@angular/material';
+import { DeleteDialogComponent } from '../delete-dialog/delete-dialog.component';
 
 @Component({
-  selector: 'app-user-list-container',
-  templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-user-list-container',
+    templateUrl: './user-list.component.html',
+    styleUrls: ['./user-list.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserListComponent implements OnInit, OnDestroy {
-  public tableConfig$: Observable<ITableConfig>;
-  public isLoading$: Observable<boolean>;
-  public availableRoles: string[];
-  public availableRoles$: Observable<string[]>;
-  public errorMessage: string;
-  public search = '';
-  public availableRolesSubscription: Subscription;
+    public tableConfig$: Observable<ITableConfig>;
+    public isLoading$: Observable<boolean>;
+    public availableRoles: string[];
+    public availableRoles$: Observable<string[]>;
+    public errorMessage: string;
+    public search = '';
+    public availableRolesSubscription: Subscription;
 
-  constructor(private userListFacade: UserListFacade, private snackBarService: SnackBarService, public dialog: MatDialog) {
-    userListFacade.initSubscriptions();
-  }
-
-  ngOnInit() {
-    this.isLoading$ = this.userListFacade.isLoading$;
-
-    this.tableConfig$ = this.userListFacade.users$.pipe(map((users: AppUser[]) => {
-      return {
-          columns: [{ title: 'user', property: 'username' }, { title: 'role', property: 'role' }, {title: 'delete', property: 'delete'}],
-          data: users
-      };
-    }));
-
-    this.availableRoles$ = this.userListFacade.availableRoles$;
-    this.availableRolesSubscription = this.userListFacade.availableRoles$.subscribe(value => this.availableRoles = value);
-  }
-
-  public onChange(user: AppUser): void {
-    this.userListFacade.updateUserRole(user.id, user.role);
-  }
-
-  public onDelete(user: AppUser): void {
-    this.userListFacade.selectedOrganizationName$
-      .pipe(
-        take(1),
-        switchMap((name: string) => {
-          return this.dialog.open(DeleteDialogComponent, {
-            width: '400px',
-            data: {
-              entityType: 'User',
-              entityName: `${user.firstName} ${user.lastName}`,
-              organizationName: name
-            }
-          }).afterClosed();
-        })
-      )
-      .subscribe(res => {
-        if (res) {
-          this.userListFacade.deleteUser(user.userId);
-        }
-      });
-  }
-
-  public onInviteUser(): void {
-    const dialog = this.dialog.open(InviteDialogComponent, {
-      data: {
-        availableRoles: this.availableRoles
-      }
-    });
-
-    const dialogSubscription = dialog.afterClosed().subscribe(({userEmail, role}) => {
-      if (userEmail && role) {
-        this.userListFacade.inviteUser(userEmail, role).subscribe(() => this.openEmailNotification(userEmail));
-        dialogSubscription.unsubscribe();
-      }
-    });
-  }
-
-  public ngOnDestroy(): void {
-    this.userListFacade.unsubscribe();
-    this.availableRolesSubscription.unsubscribe();
-  }
-
-  private openEmailNotification(email: string): void {
-    if (!email) {
-      return;
+    constructor(private userListFacade: UserListFacade, private snackBarService: SnackBarService, public dialog: MatDialog) {
+        userListFacade.initSubscriptions();
     }
 
-    this.snackBarService.openInfo(void 0, void 0, `Invitation was sent to ${email}`);
-  }
+    ngOnInit() {
+        this.isLoading$ = this.userListFacade.isLoading$;
+
+        this.tableConfig$ = this.userListFacade.users$.pipe(map((users: AppUser[]) => {
+            return {
+                columns: [{ title: 'user', property: 'username' }, {
+                    title: 'role',
+                    property: 'role'
+                }, { title: 'delete', property: 'delete' }],
+                data: users
+            };
+        }));
+
+        this.availableRoles$ = this.userListFacade.availableRoles$;
+        this.availableRolesSubscription = this.userListFacade.availableRoles$.subscribe(value => this.availableRoles = value);
+    }
+
+    public onChange(user: AppUser): void {
+        this.userListFacade.updateUserRole(user.id, user.role);
+    }
+
+    public onDelete(user: AppUser): void {
+        this.userListFacade.selectedOrganizationName$
+            .pipe(
+                take(1),
+                switchMap((name: string) => {
+                    return this.dialog.open(DeleteDialogComponent, {
+                        width: '400px',
+                        data: {
+                            entityType: 'User',
+                            entityName: `${user.firstName} ${user.lastName}`,
+                            organizationName: name
+                        }
+                    }).afterClosed();
+                }),
+                filter((isClosed: boolean) => isClosed),
+                tap(() => this.userListFacade.deleteUser(user.userId))
+
+            )
+            .subscribe();
+    }
+
+    public onInviteUser(): void {
+        const dialog = this.dialog.open(InviteDialogComponent, {
+            data: {
+                availableRoles: this.availableRoles
+            }
+        });
+
+        let userEmailValue: string = '';
+
+        dialog.afterClosed()
+            .pipe(
+                filter((data: any) => !!data),
+                tap(({ userEmail }) => userEmailValue = userEmail),
+                switchMap(({ userEmail, role }) => this.userListFacade.inviteUser(userEmail, role)),
+                tap(() => this.openEmailNotification(userEmailValue)),
+                catchError((error: HttpErrorResponse) => of(this.snackBarService.openHttpError(error))),
+            )
+            .subscribe()
+    }
+
+    public ngOnDestroy(): void {
+        this.userListFacade.unsubscribe();
+        this.availableRolesSubscription.unsubscribe();
+    }
+
+    private openEmailNotification(email: string): void {
+        if (!email) {
+            return;
+        }
+
+        this.snackBarService.openInfo(void 0, void 0, `Invitation was sent to ${email}`);
+    }
 }
