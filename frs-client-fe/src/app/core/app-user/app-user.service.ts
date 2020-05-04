a5c3dd0453e7e116a0f34bb8e3d7ac23841dbf2b
akavalchuk <akavalchuk@exadel.com>
2020-05-04 14:58:47 +0300

    EFRS-425 Wrong request type/ changed put to post

--- frs-client-fe/src/app/core/app-user/app-user.service.ts
+++ frs-client-fe/src/app/core/app-user/app-user.service.ts
@@ -24,6 +24,6 @@ export class AppUserService {
   }
 
   public inviteUser(organizationId: string, applicationId: string, userEmail: string, role: string): Observable<{message: string}> {
-    return this.http.put<{message: string}>(`${environment.apiUrl}org/${organizationId}/app/${applicationId}/invite`, { userEmail, role });
+    return this.http.post<{message: string}>(`${environment.apiUrl}org/${organizationId}/app/${applicationId}/invite`, { userEmail, role });
   }
 }
