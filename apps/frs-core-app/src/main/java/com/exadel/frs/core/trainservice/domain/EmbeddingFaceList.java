8e25d61365c13dcd6ddb7d6fe4509de8357d219f
Cheslavskyi Stanislav <scheslavskyi@exadel.com>
2020-05-07 13:35:56 +0300

    EFRS-323: review fix

--- apps/frs-core-app/src/main/java/com/exadel/frs/core/trainservice/domain/EmbeddingFaceList.java
+++ apps/frs-core-app/src/main/java/com/exadel/frs/core/trainservice/domain/EmbeddingFaceList.java
@@ -9,7 +9,6 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Data
 @Accessors(chain = true)
