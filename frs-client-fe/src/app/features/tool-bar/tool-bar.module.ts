ddc4f1195782c60c36894fdf5a67298982bfcbb2
Ivan Dodz <ivan.dodz@gmail.com>
2020-05-13 16:42:03 +0300

    EFRS-274 - make the header logo a link to the root route

--- frs-client-fe/src/app/features/tool-bar/tool-bar.module.ts
+++ frs-client-fe/src/app/features/tool-bar/tool-bar.module.ts
@@ -5,6 +5,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatIconModule} from '@angular/material/icon';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [ToolBarComponent],
@@ -16,7 +17,8 @@ import {MatIconModule} from '@angular/material/icon';
     MatToolbarModule,
     MatButtonModule,
     MatMenuModule,
-    MatIconModule
+    MatIconModule,
+    RouterModule
   ]
 })
 export class ToolBarModule { }
