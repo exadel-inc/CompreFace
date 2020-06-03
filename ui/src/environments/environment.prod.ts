310c21cafdcad73844d2ade3d6f36c51312ee451
Cheslavskyi Stanislav <scheslavskyi@exadel.com>
2020-06-03 14:32:56 +0300

    port fix

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import {Environment} from './interface';
 export const environment: Environment = {
   production: true,
   basicToken: 'Basic Q29tbW9uQ2xpZW50SWQ6cGFzc3dvcmQ=',
-  apiUrl: 'http://frs-admin:8080/admin/'
+  apiUrl: 'http://localhost:8081/admin/'
 };
