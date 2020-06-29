ae62b164ec97748b0b940f3c529b409ffc397087
Vadim Amirov <vamirov@exadel.com>
2020-06-29 12:09:22 +0500

    EFRS-610: fix duplication saving training models

--- api/src/test/java/com/exadel/frs/core/trainservice/dao/ModelDaoTest.java
+++ api/src/test/java/com/exadel/frs/core/trainservice/dao/ModelDaoTest.java
@@ -16,96 +16,102 @@
 
 package com.exadel.frs.core.trainservice.dao;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import com.exadel.frs.core.trainservice.component.classifiers.LogisticRegressionExtendedClassifier;
-import com.exadel.frs.core.trainservice.config.MongoTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+import com.exadel.frs.core.trainservice.component.classifiers.FaceClassifier;
 import com.exadel.frs.core.trainservice.entity.mongo.Model;
 import com.exadel.frs.core.trainservice.repository.mongo.ModelRepository;
-import java.util.HashMap;
+import java.util.Optional;
 import java.util.UUID;
-import lombok.extern.slf4j.Slf4j;
 import lombok.val;
-import org.apache.commons.lang3.tuple.Pair;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit.jupiter.EnabledIf;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
-@MongoTest
-@Slf4j
-@EnabledIf(expression = "#{environment.acceptsProfiles('integration-test')}")
 public class ModelDaoTest {
 
-    @Autowired
+    @Mock
     private ModelRepository modelRepository;
 
-    @AfterEach
-    public void after() {
-        modelRepository.deleteAll();
+    @InjectMocks
+    private ModelDao modelDao;
+
+    public static final String MODEL_KEY = "model_key";
+
+    @BeforeEach
+    void setUp() {
+        initMocks(this);
     }
 
     @Test
-    public void classifierSave() {
-        assertDoesNotThrow(this::saveTrainedModel);
+    void findAllWithoutClassifier() {
+        //todo
     }
 
     @Test
-    public void classifierGet() {
-        val id = saveTrainedModel();
+    void saveModel() {
+        val classifier = mock(FaceClassifier.class);
+        val calculator = "1.0";
 
-        assertTrue(modelRepository.findById(id).isPresent());
+        when(modelRepository.findFirstByModelKey(MODEL_KEY)).thenReturn(Optional.empty());
+        when(modelRepository.save(any())).then(returnsFirstArg());
+
+        val actual = modelDao.saveModel(MODEL_KEY, classifier, calculator);
+
+        assertThat(actual).isNotNull();
+        assertThat(actual.getModelKey()).isEqualTo(MODEL_KEY);
+        assertThat(actual.getClassifier()).isEqualTo(classifier);
+        assertThat(actual.getCalculatorVersion()).isEqualTo(calculator);
+
+        verify(modelRepository).findFirstByModelKey(MODEL_KEY);
+        verify(modelRepository).save(actual);
+        verifyNoMoreInteractions(modelRepository);
     }
 
     @Test
-    public void classifierGetNotFound() {
-        val id = saveTrainedModel();
-
-        assertFalse(modelRepository.findById(id + "1").isPresent());
-    }
-
-    @Test
-    public void delete() {
-        var id = saveTrainedModel();
-
-        assertEquals(1L, modelRepository.count());
-        assertDoesNotThrow(() -> modelRepository.deleteById(id));
-        assertEquals(0L, modelRepository.count());
-    }
-
-    @Test
-    public void deleteWrong() {
-        var id = saveTrainedModel();
-
-        assertEquals(1L, modelRepository.count());
-        assertDoesNotThrow(() -> modelRepository.deleteById(id + "1"));
-        assertEquals(1L, modelRepository.count());
-    }
-
-    private String saveTrainedModel() {
-        val x = new double[2][2];
-        x[0][0] = 2;
-        x[0][1] = 2;
-        x[1][0] = 3;
-        x[1][1] = 2;
-        val y = new int[2];
-        y[0] = 1;
-        y[1] = 2;
-
-        val labelMap = new HashMap<Integer, Pair<String, String>>();
-        labelMap.put(1, Pair.of(UUID.randomUUID().toString(), "firstLabel"));
-        labelMap.put(2, Pair.of(UUID.randomUUID().toString(), "secondLabel"));
-
-        val classifier = new LogisticRegressionExtendedClassifier(labelMap);
-        classifier.train(x, y);
-
+    void updateOldModel() {
+        val classifier = mock(FaceClassifier.class);
+        val calculator = "1.0";
+        val id = UUID.randomUUID().toString();
         val model = Model.builder()
-                         .classifier(classifier)
-                         .id(UUID.randomUUID().toString())
+                         .id(id)
                          .build();
 
-        return modelRepository.save(model).getId();
+        when(modelRepository.findFirstByModelKey(MODEL_KEY)).thenReturn(Optional.of(model));
+        when(modelRepository.save(any())).then(returnsFirstArg());
+
+        val actual = modelDao.saveModel(MODEL_KEY, classifier, calculator);
+
+        assertThat(actual).isNotNull();
+        assertThat(actual.getId()).isEqualTo(id);
+        assertThat(actual.getModelKey()).isEqualTo(MODEL_KEY);
+        assertThat(actual.getClassifier()).isEqualTo(classifier);
+        assertThat(actual.getCalculatorVersion()).isEqualTo(calculator);
+
+        verify(modelRepository).findFirstByModelKey(MODEL_KEY);
+        verify(modelRepository).save(actual);
+        verifyNoMoreInteractions(modelRepository);
+    }
+
+    @Test
+    void updateModelApiKey() {
+        //todo
+    }
+
+    @Test
+    void getModel() {
+        //todo
+    }
+
+    @Test
+    void deleteModel() {
+        //todo
     }
 }
