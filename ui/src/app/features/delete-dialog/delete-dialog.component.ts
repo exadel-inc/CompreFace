240a4f260e99414dd636db51fe8b90e390b0702e
Alex Kravets <okravets@exadel.com>
2020-07-23 13:13:54 +0300

    removed unused import

--- ui/src/app/features/delete-dialog/delete-dialog.component.ts
+++ ui/src/app/features/delete-dialog/delete-dialog.component.ts
@@ -13,7 +13,7 @@
  * or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
-import {Component, Inject, Input, OnInit} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
