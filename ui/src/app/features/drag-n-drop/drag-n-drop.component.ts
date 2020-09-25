fc306a245f2c2bcb40c6ba228a4df7b9cd87ec46
mjakowiec <mjakowiec@exadel.com>
2020-09-25 14:24:25 +0200

    EFRS-590 Paint result on canvas

--- ui/src/app/features/drag-n-drop/drag-n-drop.component.ts
+++ ui/src/app/features/drag-n-drop/drag-n-drop.component.ts
@@ -32,6 +32,8 @@ export class DragNDropComponent {
   @Input() loading = true;
   @Input() model: Model;
   @Output() recognizeFace = new EventEmitter();
+  @ViewChild('myCanvas', { static: true }) myCanvas: ElementRef;
+
 
   constructor(private dragService: DragNDropService) {}
 
@@ -59,5 +61,30 @@ export class DragNDropComponent {
       this.file = item;
     }
     this.recognizeFace.emit(this.file, this.model);
+  }
+
+  printResult(box: any, face: any) {
+    const img = new Image();
+    const ctx: CanvasRenderingContext2D =
+    this.myCanvas.nativeElement.getContext('2d');
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0);
+      ctx.beginPath();
+      ctx.strokeStyle = 'green';
+      ctx.moveTo(box.x_min, box.y_min);
+      ctx.lineTo(box.x_max, box.y_min);
+      ctx.lineTo(box.x_max, box.y_max);
+      ctx.lineTo(box.x_min, box.y_max);
+      ctx.lineTo(box.x_min, box.y_min);
+      ctx.stroke();
+      ctx.fillStyle = 'green';
+      ctx.fillRect(box.x_min, box.y_min - 25, 200, 25);
+      ctx.fillRect(box.x_min, box.y_max, 200, 25);
+      ctx.fillStyle = 'white';
+      ctx.font = '20pt Roboto Regular Helvetica Neue sans-serif';
+      ctx.fillText(box.probability, box.x_min + 10, box.y_max + 20);
+      ctx.fillText(face[0].face_name, box.x_min + 10, box.y_min - 5);
+    };
+    img.src = URL.createObjectURL(this.file);
   }
 }
