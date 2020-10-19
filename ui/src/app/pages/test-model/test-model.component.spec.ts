715a794ce640feda459f7b12fa1bc39158af042d
Sisha3342 <spalanevich@mail.ru>
2020-10-20 01:10:24 +0300

    EFRS-811: Added mock services for TestModelComponent

--- ui/src/app/pages/test-model/test-model.component.spec.ts
+++ ui/src/app/pages/test-model/test-model.component.spec.ts
@@ -1,25 +1,36 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TestModelComponent } from './test-model.component';
+import {TestModelPageService } from "./test-model.service";
+import {ComponentFixture, TestBed, waitForAsync} from "@angular/core/testing";
+import {Store} from "@ngrx/store";
+
+class MockTestModelPageService {
+  initUrlBindingStreams() {};
+}
+
+class MockStore {
+  select() {};
+}
 
 describe('TestModelComponent', () => {
   let component: TestModelComponent;
   let fixture: ComponentFixture<TestModelComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ TestModelComponent ]
-    })
-    .compileComponents();
+      declarations: [ TestModelComponent ],
+      providers: [ {provide: TestModelPageService, useValue: new MockTestModelPageService()},
+        { provide: Store, useClass: MockStore}],
+    }).compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(waitForAsync(() => {
+    TestBed.overrideProvider(Store, {useValue: new MockStore()})
     fixture = TestBed.createComponent(TestModelComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeDefined();
   });
 });
