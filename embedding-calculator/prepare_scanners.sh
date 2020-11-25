9c943b5ca9e52ebb31cb66d8b58325bbf9dcb099
Andrey Frolov <zeratul268@gmail.com>
2020-11-25 14:50:35 +0500

    EFRS-873 Create new endpoint for face detection (#227)
    EFRS-873 Create new endpoint for face detection

--- embedding-calculator/prepare_scanners.sh
+++ embedding-calculator/prepare_scanners.sh
@@ -5,9 +5,11 @@ if [[ "$SCANNER" == "InsightFace" ]]; then
   mkdir -p $MODELS_PATH
   for MODEL in $DETECTION_MODEL $CALCULATION_MODEL
   do
+    # trying to find a pre-downloaded model
     DIR=~/srcext/insightface/models/$MODEL
-    [ -d "$DIR" ] && echo "Coping $MODEL from repo..." && cp -r $DIR $MODELS_PATH && continue
+    [ -d "$DIR" ] && echo "Coping $MODEL from repository..." && cp -r $DIR $MODELS_PATH && continue
 
+    # download a model
     URL=http://insightface.ai/files/models/$MODEL.zip
     echo "Downloading $URL..."
     mkdir -p $MODELS_PATH/$MODEL && cd "$_" && curl -L $URL -o m.zip  \
@@ -15,4 +17,10 @@ if [[ "$SCANNER" == "InsightFace" ]]; then
   done
 else
   echo "  --ignore=src/services/facescan/scanner/insightface" >> pytest.ini
+fi
+
+if [[ "$SCANNER" == "Facenet2018" ]]; then
+  pip install --no-cache-dir tensorflow~=1.15.4
+else
+  echo "  --ignore=src/services/facescan/scanner/facenet" >> pytest.ini
 fi
--- embedding-calculator/src/_endpoints.py
+++ embedding-calculator/src/_endpoints.py
@@ -11,14 +11,15 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
+from typing import List
 
+from flask import request
 from flask.json import jsonify
 from werkzeug.exceptions import BadRequest
 
-from src.cache import get_scanner
 from src.constants import ENV
 from src.exceptions import NoFaceFoundError
-from src.services.facescan.scanner.facescanner import FaceScanner
+from src.services.facescan.scanner.facescanners import scanner
 from src.services.flask_.constants import ARG
 from src.services.flask_.needs_attached_file import needs_attached_file
 from src.services.imgtools.read_img import read_img
@@ -27,20 +28,28 @@ from src.services.imgtools.read_img import read_img
 def endpoints(app):
     @app.route('/status')
     def status_get():
-        return jsonify(status='OK', build_version=ENV.BUILD_VERSION, calculator_version=ENV.SCANNER)
+        return jsonify(status='OK', build_version=ENV.BUILD_VERSION,
+                       calculator_version=ENV.SCANNER)
+
+    @app.route('/find_faces', methods=['POST'])
+    @needs_attached_file
+    def find_faces_post():
+        faces = scanner.find_faces(
+            img=read_img(request.files['file']),
+            det_prob_threshold=_get_det_prob_threshold(request),
+        )
+        faces = _limit(faces, request.values.get(ARG.LIMIT))
+        return jsonify(calculator_version=scanner.ID, result=faces)
 
     @app.route('/scan_faces', methods=['POST'])
     @needs_attached_file
     def scan_faces_post():
-        from flask import request
-        img = read_img(request.files['file'])
-        limit_faces = _get_limit_faces_fun(request.values.get(ARG.LIMIT))
-        scanner: FaceScanner = get_scanner()
-        det_prob_threshold = _get_det_prob_threshold(request)
-
-        scanned_faces = scanner.scan(img, det_prob_threshold)
-
-        return jsonify(calculator_version=get_scanner().ID, result=limit_faces(_at_least_one_face(scanned_faces)))
+        faces = scanner.scan(
+            img=read_img(request.files['file']),
+            det_prob_threshold=_get_det_prob_threshold(request)
+        )
+        faces = _limit(faces, request.values.get(ARG.LIMIT))
+        return jsonify(calculator_version=scanner.ID, result=faces)
 
 
 def _get_det_prob_threshold(request):
@@ -53,34 +62,27 @@ def _get_det_prob_threshold(request):
     return det_prob_threshold
 
 
-def _get_limit_faces_fun(limit_arg: int):
+def _limit(faces: List, limit: str = None) -> List:
     """
-    >>> _get_limit_faces_fun(None)([1, 2, 3])
+    >>> _limit([1, 2, 3], None)
     [1, 2, 3]
-    >>> _get_limit_faces_fun('')([1, 2, 3])
+    >>> _limit([1, 2, 3], '')
     [1, 2, 3]
-    >>> _get_limit_faces_fun(0)([1, 2, 3])
+    >>> _limit([1, 2, 3], 0)
     [1, 2, 3]
-    >>> _get_limit_faces_fun(1)([1, 2, 3])
+    >>> _limit([1, 2, 3], 1)
     [1]
-    >>> _get_limit_faces_fun(2)([1, 2, 3])
+    >>> _limit([1, 2, 3], 2)
     [1, 2]
     """
-    if limit_arg is None or limit_arg == '':
-        return lambda lst: lst
+    if len(faces) == 0:
+        raise NoFaceFoundError
 
     try:
-        limit = int(limit_arg)
+        limit = int(limit or 0)
     except ValueError as e:
         raise BadRequest('Limit format is invalid (limit >= 0)') from e
-
     if not (limit >= 0):
         raise BadRequest('Limit value is invalid (limit >= 0)')
 
-    return lambda lst: lst[:limit] if limit else lst
-
-
-def _at_least_one_face(result):
-    if len(result) == 0:
-        raise NoFaceFoundError
-    return result
+    return faces[:limit] if limit else faces
