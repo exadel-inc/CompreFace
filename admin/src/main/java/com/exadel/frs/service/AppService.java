d65d78014627a6665f1e4c9e1ea0eb99427500d4
Aliaksandr Pleski <apleski@exadel.com>
2020-12-17 12:58:42 +0300

    EFRS-887: Different logic on UI and Backend for Global User - App Admin role. - fixed admin's privileges for working with Models

--- admin/src/main/java/com/exadel/frs/service/AppService.java
+++ admin/src/main/java/com/exadel/frs/service/AppService.java
@@ -153,7 +153,7 @@ public class AppService {
         val user = userService.getUser(userInviteDto.getUserEmail());
         val admin = userService.getUser(userId);
 
-        authManager.verifyWritePrivilegesToApp(admin, app);
+        authManager.verifyWritePrivilegesToApp(admin, app, true);
 
         val userAppRole = app.getUserAppRole(user.getId());
         if (userAppRole.isPresent()) {
@@ -193,7 +193,7 @@ public class AppService {
         val app = getApp(appGuid, userId);
         val user = userService.getUser(userId);
 
-        authManager.verifyWritePrivilegesToApp(user, app);
+        authManager.verifyWritePrivilegesToApp(user, app, true);
 
         val isSameName = app.getName().equals(appUpdateDto.getName());
         if (isNotTrue(isSameName)) {
@@ -208,7 +208,7 @@ public class AppService {
         val app = getApp(guid, adminId);
         val admin = userService.getUser(adminId);
 
-        authManager.verifyWritePrivilegesToApp(admin, app);
+        authManager.verifyWritePrivilegesToApp(admin, app, true);
 
         val userToUpdate = userService.getUserByGuid(userRoleUpdateDto.getUserId());
         if (userToUpdate.getId().equals(adminId)) {
@@ -251,7 +251,7 @@ public class AppService {
         val app = getApp(guid, userId);
         val user = userService.getUser(userId);
 
-        authManager.verifyWritePrivilegesToApp(user, app);
+        authManager.verifyWritePrivilegesToApp(user, app, true);
 
         app.setApiKey(UUID.randomUUID().toString());
 
@@ -263,7 +263,7 @@ public class AppService {
         val app = getApp(guid, userId);
         val user = userService.getUser(userId);
 
-        authManager.verifyWritePrivilegesToApp(user, app);
+        authManager.verifyWritePrivilegesToApp(user, app, true);
 
         appRepository.deleteById(app.getId());
     }
@@ -272,7 +272,7 @@ public class AppService {
         val app = getApp(appGuid);
         val user = userService.getUser(SecurityUtils.getPrincipalId());
 
-        authManager.verifyWritePrivilegesToApp(user, app);
+        authManager.verifyWritePrivilegesToApp(user, app, true);
 
         val requestId = UUID.randomUUID();
         val id = ModelShareRequestId.builder()
