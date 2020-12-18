ef2feef84c710ae558002f610ca117c35d4673d6
Pavlo <adamdoom23@gmail.com>
2020-12-18 15:36:15 +0200

    Removed redundant argument

--- ui/src/app/features/user-list/user-list.component.ts
+++ ui/src/app/features/user-list/user-list.component.ts
@@ -110,7 +110,7 @@ export class UserListComponent implements OnInit, OnDestroy {
             .afterClosed();
         }),
         filter((isClosed: boolean) => isClosed),
-        tap(() => this.userListFacade.deleteUser(deletion, this.seletedOption))
+        tap(() => this.userListFacade.deleteUser(deletion))
       )
       .subscribe();
   }
