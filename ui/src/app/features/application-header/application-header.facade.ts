9b90b423ac605f79229fc12d0e9dc6a43cef274c
Pavlo <adamdoom23@gmail.com>
2021-01-08 16:38:03 +0200

    EFRS-896 - Fixed application header facade and module

--- ui/src/app/features/application-header/application-header.facade.ts
+++ ui/src/app/features/application-header/application-header.facade.ts
@@ -13,6 +13,7 @@
  * or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { combineLatest, Observable, Subscription } from 'rxjs';
@@ -24,35 +25,28 @@ import { Application } from '../../data/interfaces/application';
 import { IFacade } from '../../data/interfaces/IFacade';
 import { AppState } from '../../store';
 import { deleteApplication, updateApplication } from '../../store/application/action';
-import {
-  selectCurrentApp,
-  selectCurrentAppId,
-  selectIsPendingApplicationList,
-  selectUserRollForSelectedApp,
-} from '../../store/application/selectors';
+import { selectCurrentApp, selectCurrentAppId, selectUserRollForSelectedApp } from '../../store/application/selectors';
+import { selectIsLoadingApplicationList } from '../../store/user/selectors';
 
 @Injectable()
 export class ApplicationHeaderFacade implements IFacade {
   selectedId$: Observable<string | null>;
   selectedId: string | null;
-  isLoading$: Observable<boolean>;
+  isLoadingAppList$: Observable<boolean>;
   userRole$: Observable<string | null>;
-  currentUserRole$: Observable<string | null>;
   app$: Observable<Application>;
   appIdSub: Subscription;
 
   constructor(private store: Store<AppState>) {
-    this.app$ = this.store.select(selectCurrentApp);
-    this.currentUserRole$ = this.store.select(selectCurrentUserRole);
-    this.userRole$ = combineLatest([this.store.select(selectUserRollForSelectedApp), this.currentUserRole$]).pipe(
-      map(([applicationRole, globalRole]) =>
+    this.app$ = store.select(selectCurrentApp);
+    this.selectedId$ = store.select(selectCurrentAppId);
+    this.isLoadingAppList$ = store.select(selectIsLoadingApplicationList);
+    this.userRole$ = combineLatest([store.select(selectUserRollForSelectedApp), store.select(selectCurrentUserRole)]).pipe(
+      map(([applicationRole, organizationRole]) =>
         // the global role (if OWNER or ADMINISTRATOR) should prevail on the application role
-        globalRole !== Role.User ? Role.Owner : applicationRole
+        organizationRole !== Role.User ? Role.Owner : applicationRole
       )
     );
-    this.selectedId$ = this.store.select(selectCurrentAppId);
-    this.isLoading$ = combineLatest([this.store.select(selectIsPendingApplicationList), this.currentUserRole$]).pipe(
-      map(([pendingApplicationList, currentUserRole]) => (!pendingApplicationList && !currentUserRole)));
   }
 
   initSubscriptions() {
