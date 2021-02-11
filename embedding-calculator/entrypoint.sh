e825556ac8f7d8c5808f913477ef88353ef9adc9
Asim <ahashymau@exadel.com>
2021-02-11 10:44:34 +0300

    EFRS-907: added recognition_optimization key to enable Intel optimization

--- embedding-calculator/entrypoint.sh
+++ embedding-calculator/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ "$ENABLE_RECOGNITION_OPTIMIZATION" = "true" ]; \
+if [ "$RECOGNITION_OPTIMIZATION" = "intel_mkl" ]; \
     then export GPU_IDX="$GPU_IDX" INTEL_OPTIMIZATION="true"; \
     else export GPU_IDX="-1" INTEL_OPTIMIZATION="false"; \
 fi
