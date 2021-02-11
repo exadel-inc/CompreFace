1983221b05df3b071ed4bf5a29ed5efce40cdbc2
Asim <ahashymau@exadel.com>
2021-02-11 17:24:27 +0300

    Added ND4J_CLASSIFIER that defines ND4J classifier on maven build

--- embedding-calculator/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ "$RECOGNITION_OPTIMIZATION" = "intel_mkl" ]; \
-    then export GPU_IDX="$GPU_IDX" INTEL_OPTIMIZATION="true"; \
-    else export GPU_IDX="-1" INTEL_OPTIMIZATION="false"; \
-fi
-
-uwsgi --ini uwsgi.ini
