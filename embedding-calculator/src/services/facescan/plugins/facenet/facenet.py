fa71ff1881e5cb7fb426af9eabb92760be03ab93
spospielov <spospielov@exadel.com>
2021-03-14 14:00:32 +0200

    Updated coefficients

--- embedding-calculator/src/services/facescan/plugins/facenet/facenet.py
+++ embedding-calculator/src/services/facescan/plugins/facenet/facenet.py
@@ -109,7 +109,7 @@ class Calculator(mixins.CalculatorMixin, base.BasePlugin):
         # VGGFace2 training set, 0.9965 LFW accuracy
         ('20180402-114759', '1im5Qq006ZEV_tViKh3cgia_Q4jJ13bRK', (1.1817961, 5.291995557), 0.4),
         # CASIA-WebFace training set, 0.9905 LFW accuracy
-        ('20180408-102900', '100w4JIUz44Tkwte9F-wEH0DOFsY-bPaw', (1.1817961, 5.291995557), 0.4),
+        ('20180408-102900', '100w4JIUz44Tkwte9F-wEH0DOFsY-bPaw', (1.1362496, 5.803152427), 0.4),
     )
     BATCH_SIZE = 25
 
