9434b98e7e638e15a6a1dd2f43c1adf77f10a3fa
Pavel Bohdan <pbohdan@exadel.com>
2021-07-06 09:32:08 +0300

    Added modal window.

---
+++ ui/src/app/features/collection-manager-subject-left/collection-manager-subject-left..ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class CollectionManagerSubjectLeft {
+  constructor() {}
+
+  createSubject(apiKey: string, subject: string) {}
+}
