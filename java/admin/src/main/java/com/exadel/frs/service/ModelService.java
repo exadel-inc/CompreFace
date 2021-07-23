93e8f363a1145007e3c4b390bdddbe52cb1058c8
ksidikov <ksidikov@exadel.com>
2021-07-23 17:52:37 +0500

    EFRS-1102: removed unneeded import

--- java/admin/src/main/java/com/exadel/frs/service/ModelService.java
+++ java/admin/src/main/java/com/exadel/frs/service/ModelService.java
@@ -34,7 +34,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
