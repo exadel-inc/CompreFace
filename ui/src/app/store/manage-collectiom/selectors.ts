664366daa2deaf8d1117a7cb294b65373a63b0e5
spospielov <spospielov@exadel.com>
2021-08-29 17:47:35 +0300

    removed typo

--- ui/src/app/store/manage-collectiom/selectors.ts
+++ ui/src/app/store/manage-collectiom/selectors.ts
@@ -6,7 +6,6 @@
  * You may obtain a copy of the License at
  *
  *      https://www.apache.org/licenses/LICENSE-2.0
- cdk-overlay-connected-position-bounding-box
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
