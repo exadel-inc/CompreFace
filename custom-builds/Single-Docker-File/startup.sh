e9ed7ec3df2c876e24510c37fabe245996b6918c
spospielov <spospielov@exadel.com>
2021-12-12 17:19:57 +0200

    added ability not to run internal DB

--- custom-builds/Single-Docker-File/startup.sh
+++ custom-builds/Single-Docker-File/startup.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-# restore default data if it was cleared by volume creation
-if [ -z "$(ls -A $PGDATA)" ]; then
-   echo "Postgres directory is empty. Copy default values into it"
-   cp -r /var/lib/postgresql/default/* $PGDATA
-fi
-# change permissions in case they were corrupted
-chown -R postgres:postgres $PGDATA
-chmod 700 $PGDATA
+# EXTERNAL_DB defines if we need to run internal DB
+external_db=${EXTERNAL_DB:-false}
+if [ "$external_db" = false ] ; then
+    # restore default data if it was cleared by volume creation
+    if [ -z "$(ls -A $PGDATA)" ]; then
+       echo "Postgres directory is empty. Copy default values into it"
+       cp -r /var/lib/postgresql/default/* $PGDATA
+    fi
+    # change permissions in case they were corrupted
+    chown -R postgres:postgres $PGDATA
+    chmod 700 $PGDATA
 
-echo Starting compreface-postgres-db
-supervisorctl start compreface-postgres-db
+    echo Starting compreface-postgres-db
+    supervisorctl start compreface-postgres-db
+fi
 
 # wait until DB starts
 sleep 10
