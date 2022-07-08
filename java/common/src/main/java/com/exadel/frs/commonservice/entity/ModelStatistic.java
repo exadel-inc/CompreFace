6bd5fcfa1c289dd192d02c2fd141de2d4c871d43
Volodymyr Bushko <volodymyr.bushko@gmail.com>
2022-07-08 11:13:00 +0300

    EFRS-1256: Optimized database query performance

--- java/common/src/main/java/com/exadel/frs/commonservice/entity/ModelStatistic.java
+++ java/common/src/main/java/com/exadel/frs/commonservice/entity/ModelStatistic.java
@@ -4,6 +4,7 @@ import static javax.persistence.GenerationType.SEQUENCE;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -34,7 +35,7 @@ public class ModelStatistic {
     @Column(name = "request_count")
     private Integer requestCount;
 
-    @ManyToOne(optional = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "model_id", referencedColumnName = "id")
     private Model model;
 
