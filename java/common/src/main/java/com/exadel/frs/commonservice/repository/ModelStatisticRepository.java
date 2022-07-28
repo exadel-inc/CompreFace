ca110dcf94ed2fc5e6bdaab0b8ed64e2e175eb4e
Volodymyr Bushko <volodymyr.bushko@gmail.com>
2022-07-28 09:29:48 +0300

    EFRS-1268: Added an endpoint to return model statistics

--- java/common/src/main/java/com/exadel/frs/commonservice/repository/ModelStatisticRepository.java
+++ java/common/src/main/java/com/exadel/frs/commonservice/repository/ModelStatisticRepository.java
@@ -1,7 +1,9 @@
 package com.exadel.frs.commonservice.repository;
 
 import com.exadel.frs.commonservice.entity.ModelStatistic;
+import com.exadel.frs.commonservice.entity.ModelStatisticProjection;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +13,6 @@ import org.springframework.stereotype.Repository;
 public interface ModelStatisticRepository extends JpaRepository<ModelStatistic, Long> {
 
     Stream<ModelStatistic> findAllByModelIdInAndCreatedDate(Set<Long> modelIds, LocalDateTime createdDate);
+
+    List<ModelStatisticProjection> findAllByModelGuidAndCreatedDateBetween(String modelGuid, LocalDateTime from, LocalDateTime to);
 }
