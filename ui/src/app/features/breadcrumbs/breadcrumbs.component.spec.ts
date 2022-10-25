590ff86f1bf4b4701359a7d2f473c86c28dc4cff
Saba <69626492+smchedlidze826@users.noreply.github.com>
2022-10-25 23:31:43 +0400

    failing test fixed in 1.1.x (#930)

--- ui/src/app/features/breadcrumbs/breadcrumbs.component.spec.ts
+++ ui/src/app/features/breadcrumbs/breadcrumbs.component.spec.ts
@@ -19,6 +19,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateService } from '@ngx-translate/core';
 
 import { BreadcrumbsComponent } from './breadcrumbs.component';
 import { BreadcrumbsFacade } from './breadcrumbs.facade';
@@ -27,20 +28,16 @@ describe('BreadcrumbsComponent', () => {
   let component: BreadcrumbsComponent;
   let fixture: ComponentFixture<BreadcrumbsComponent>;
 
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        declarations: [BreadcrumbsComponent],
-        providers: [
-          {
-            provide: BreadcrumbsFacade,
-            useValue: {},
-          },
-        ],
-        imports: [CommonModule, RouterModule, RouterTestingModule, MatCardModule, MatDialogModule],
-      }).compileComponents();
-    })
-  );
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [BreadcrumbsComponent],
+      providers: [
+        { provide: BreadcrumbsFacade, useValue: {} },
+        { provide: TranslateService, useValue: {} },
+      ],
+      imports: [CommonModule, RouterModule, RouterTestingModule, MatCardModule, MatDialogModule],
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BreadcrumbsComponent);
