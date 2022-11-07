bb7c077b26a9f71436e1da2365dacfab2b6f322a
Volodymyr Bushko <volodymyr.bushko@gmail.com>
2022-11-07 17:37:24 +0200

    EFRS-1286: Added a custom change in order to set expiration date for old oauth tokens

---
+++ java/common/src/main/java/com/exadel/frs/commonservice/system/liquibase/customchange/SetOAuthTokenExpirationCustomChange.java
@@ -0,0 +1,108 @@
+package com.exadel.frs.commonservice.system.liquibase.customchange;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import liquibase.change.custom.CustomTaskChange;
+import liquibase.database.Database;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.CustomChangeException;
+import liquibase.exception.DatabaseException;
+import liquibase.exception.SetupException;
+import liquibase.exception.ValidationErrors;
+import liquibase.resource.ResourceAccessor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+@Setter
+public class SetOAuthTokenExpirationCustomChange implements CustomTaskChange {
+
+    private static final String REFRESH_TOKEN = "refresh_token";
+
+    private static final String SET_ACCESS_TOKEN_EXPIRATION_SQL = "UPDATE oauth_access_token SET expiration = ? WHERE client_id = ?";
+    private static final String SET_REFRESH_TOKEN_EXPIRATION_SQL = "UPDATE oauth_refresh_token SET expiration = ? WHERE token_id IN (SELECT refresh_token FROM oauth_access_token WHERE client_id = ?)";
+
+    private String clientId;
+    private Integer accessTokenValidity;
+    private Integer refreshTokenValidity;
+    private String authorizedGrantTypes;
+
+    @Override
+    public void execute(final Database database) throws CustomChangeException {
+        try {
+            JdbcConnection connection = (JdbcConnection) database.getConnection();
+            setAccessTokenExpiration(connection);
+            if (authorizedGrantTypes.contains(REFRESH_TOKEN)) {
+                setRefreshTokenExpiration(connection);
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new CustomChangeException(e);
+        }
+    }
+
+    private void setAccessTokenExpiration(final JdbcConnection connection)
+            throws DatabaseException, SQLException {
+        int updateCount = setTokenExpiration(
+                connection,
+                SET_ACCESS_TOKEN_EXPIRATION_SQL,
+                accessTokenValidity
+        );
+        log.info(
+                "Updated {} access tokens for client {}",
+                updateCount,
+                clientId
+        );
+    }
+
+    private void setRefreshTokenExpiration(final JdbcConnection connection)
+            throws DatabaseException, SQLException {
+        int updateCount = setTokenExpiration(
+                connection,
+                SET_REFRESH_TOKEN_EXPIRATION_SQL,
+                refreshTokenValidity
+        );
+        log.info(
+                "Updated {} refresh tokens for client {}",
+                updateCount,
+                clientId
+        );
+    }
+
+    private int setTokenExpiration(final JdbcConnection connection, final String sql, final long tokenValidity)
+            throws DatabaseException, SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            Date expiration = calculateExpiration(tokenValidity);
+            statement.setDate(1, expiration);
+            statement.setString(2, clientId);
+            return statement.executeUpdate();
+        }
+    }
+
+    private Date calculateExpiration(final long validity) {
+        return new Date((validity - System.currentTimeMillis()) / 1000L);
+    }
+
+    @Override
+    public String getConfirmationMessage() {
+        return null;
+    }
+
+    @Override
+    public void setUp() throws SetupException {
+
+    }
+
+    @Override
+    public void setFileOpener(final ResourceAccessor resourceAccessor) {
+
+    }
+
+    @Override
+    public ValidationErrors validate(final Database database) {
+        return null;
+    }
+}
