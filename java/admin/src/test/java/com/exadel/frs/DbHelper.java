1d44cc0bb8a38e1262300ae68c4e31dd0ff7de9e
Volodymyr Bushko <volodymyr.bushko@gmail.com>
2022-12-06 12:05:47 +0200

    EFRS-1348: Added tests for StatisticService

--- java/admin/src/test/java/com/exadel/frs/DbHelper.java
+++ java/admin/src/test/java/com/exadel/frs/DbHelper.java
@@ -1,5 +1,15 @@
 package com.exadel.frs;
 
+import static com.exadel.frs.ItemsBuilder.makeApp;
+import static com.exadel.frs.ItemsBuilder.makeEmbedding;
+import static com.exadel.frs.ItemsBuilder.makeImg;
+import static com.exadel.frs.ItemsBuilder.makeModel;
+import static com.exadel.frs.ItemsBuilder.makeSubject;
+import static com.exadel.frs.commonservice.enums.GlobalRole.USER;
+import static java.time.LocalDateTime.now;
+import static java.time.ZoneOffset.UTC;
+import static java.time.temporal.ChronoUnit.MILLIS;
+import static java.util.UUID.randomUUID;
 import com.exadel.frs.commonservice.entity.Embedding;
 import com.exadel.frs.commonservice.entity.Img;
 import com.exadel.frs.commonservice.entity.Model;
@@ -7,40 +17,22 @@ import com.exadel.frs.commonservice.entity.ModelStatistic;
 import com.exadel.frs.commonservice.entity.ResetPasswordToken;
 import com.exadel.frs.commonservice.entity.Subject;
 import com.exadel.frs.commonservice.entity.User;
+import com.exadel.frs.commonservice.enums.GlobalRole;
 import com.exadel.frs.commonservice.enums.ModelType;
 import com.exadel.frs.commonservice.repository.EmbeddingRepository;
 import com.exadel.frs.commonservice.repository.ImgRepository;
 import com.exadel.frs.commonservice.repository.ModelRepository;
 import com.exadel.frs.commonservice.repository.ModelStatisticRepository;
-import com.exadel.frs.commonservice.repository.ModelStatisticRepository;
 import com.exadel.frs.commonservice.repository.SubjectRepository;
 import com.exadel.frs.commonservice.repository.UserRepository;
-import com.exadel.frs.dto.ui.UserCreateDto;
 import com.exadel.frs.repository.AppRepository;
-import com.exadel.frs.service.UserService;
-import java.time.LocalDateTime;
 import com.exadel.frs.repository.ResetPasswordTokenRepository;
-import com.exadel.frs.service.UserService;
 import java.time.LocalDateTime;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
-
-import static com.exadel.frs.ItemsBuilder.*;
-import static com.exadel.frs.commonservice.enums.GlobalRole.OWNER;
-import static com.exadel.frs.commonservice.enums.GlobalRole.USER;
-import static java.time.LocalDateTime.now;
-import static java.time.ZoneOffset.UTC;
-import static java.time.temporal.ChronoUnit.MILLIS;
-import static java.util.UUID.randomUUID;
-import static com.exadel.frs.commonservice.enums.GlobalRole.USER;
-import static java.time.LocalDateTime.now;
-import static java.time.ZoneOffset.UTC;
-import static java.time.temporal.ChronoUnit.MILLIS;
-import static java.util.UUID.randomUUID;
 
 @Service
 // TODO think about common helper for admin/core
@@ -153,18 +145,26 @@ public class DbHelper {
     }
 
     public User insertUser(String email) {
-        var user = createUser(email);
+        return insertUser(email, USER);
+    }
+
+    public User insertUser(String email, GlobalRole role) {
+        var user = createUser(email, role);
         user.setEnabled(true);
         return userRepository.saveAndFlush(user);
     }
 
     public User insertUnconfirmedUser(String email) {
-        var user = createUser(email);
+        return insertUnconfirmedUser(email, USER);
+    }
+
+    public User insertUnconfirmedUser(String email, GlobalRole role) {
+        var user = createUser(email, role);
         user.setRegistrationToken(UUID.randomUUID().toString());
         return userRepository.saveAndFlush(user);
     }
 
-    private User createUser(String email) {
+    private User createUser(String email, GlobalRole role) {
         return User.builder()
                    .email(email)
                    .firstName("firstName")
@@ -174,8 +174,8 @@ public class DbHelper {
                    .accountNonExpired(true)
                    .accountNonLocked(true)
                    .credentialsNonExpired(true)
-                   .allowStatistics(false)
-                   .globalRole(USER)
+                   .allowStatistics(true)
+                   .globalRole(role)
                    .build();
     }
 
